1545c1e2665f1ca1a9f6d704e5b0ed3c02c51a80
Dan Freeman <dfreeman@salsify.com>
2020-07-26 12:30:03 +0200

    Pull `GlintConfig` into a separate module

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import { cosmiconfigSync } from 'cosmiconfig';
-import { Minimatch, IMinimatch } from 'minimatch';
+import { GlintConfig } from './config';
 
 /**
  * Loads glint configuration, starting from the given directory
@@ -15,71 +15,4 @@ export function loadConfig(from: string): GlintConfig {
   return new GlintConfig(from);
 }
 
-export class GlintConfig {
-  public readonly rootDir: string;
 
-  private includeMatchers: Array<IMinimatch>;
-  private excludeMatchers: Array<IMinimatch>;
-
-  public constructor(rootDir: string, config: Record<string, unknown> = {}) {
-    validateInput(config);
-
-    this.rootDir = normalizePath(rootDir);
-
-    let include = Array.isArray(config.include) ? config.include : [config.include ?? '**/*.ts'];
-    let exclude = Array.isArray(config.exclude)
-      ? config.exclude
-      : [config.exclude ?? '**/node_modules/**'];
-
-    this.includeMatchers = this.buildMatchers(include);
-    this.excludeMatchers = this.buildMatchers(exclude);
-  }
-
-  public includesFile(rawFileName: string): boolean {
-    let fileName = normalizePath(rawFileName);
-
-    return (
-      this.excludeMatchers.every((matcher) => !matcher.match(fileName)) &&
-      this.includeMatchers.some((matcher) => matcher.match(fileName))
-    );
-  }
-
-  private buildMatchers(globs: Array<string>): Array<IMinimatch> {
-    return globs.map((glob) => new Minimatch(normalizePath(path.resolve(this.rootDir, glob))));
-  }
-}
-
-export type GlintConfigInput = {
-  include?: string | Array<string>;
-  exclude?: string | Array<string>;
-};
-
-function validateInput(input: Record<string, unknown>): asserts input is GlintConfigInput {
-  assert(
-    Array.isArray(input.include)
-      ? input.include.every((item) => typeof item === 'string')
-      : !input.include || typeof input.include === 'string',
-    'If defined, `include` must be a string or array of strings'
-  );
-
-  assert(
-    Array.isArray(input.exclude)
-      ? input.exclude.every((item) => typeof item === 'string')
-      : !input.exclude || typeof input.exclude === 'string',
-    'If defined, `exclude` must be a string or array of strings'
-  );
-}
-
-function assert(test: unknown, message: string): asserts test {
-  if (!test) {
-    throw new Error(`@glint/config: ${message}`);
-  }
-}
-
-export function normalizePath(fileName: string): string {
-  if (path.sep !== '/') {
-    return fileName.split(path.sep).join('/');
-  }
-
-  return fileName;
-}
