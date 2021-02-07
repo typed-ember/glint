bb5953e5ac3635bfa906f497b4f068e4fbe8eb4a
Dan Freeman <dfreeman@salsify.com>
2021-02-07 23:39:47 +0100

    Tweak @glint/core test setup for language server tests

--- packages/core/jest.config.js
+++ packages/core/jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.ts'],
+  // Don't restart tests when the source changes; wait and let
+  // tsc emitting new .js files trigger a run instead.
+  watchPathIgnorePatterns: ['<rootDir>/src/.*'],
 };
