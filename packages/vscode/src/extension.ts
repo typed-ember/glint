321f4ada614cbefdbe70c824d34f0603407d957a
Dan Freeman <dfreeman@salsify.com>
2021-02-26 16:11:17 +0100

    Support multi-root workspaces

--- packages/vscode/src/extension.ts
+++ packages/vscode/src/extension.ts
@@ -1,35 +1,71 @@
-import { ExtensionContext, workspace } from 'vscode';
-import { LanguageClient, LanguageClientOptions } from 'vscode-languageclient/node';
+import { ExtensionContext, workspace, window, WorkspaceFolder } from 'vscode';
+import {
+  Disposable,
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+} from 'vscode-languageclient/node';
+import { sync as resolve } from 'resolve';
 
 module.exports = {
   activate(context: ExtensionContext) {
-    const executable = {
-      command: 'node_modules/.bin/glint-language-server',
-    };
-
-    const serverOptions = {
-      run: executable,
-      debug: executable,
-    };
-
-    const clientOptions: LanguageClientOptions = {
-      documentSelector: [
-        {
-          scheme: 'file',
-          language: 'handlebars',
-        },
-        {
-          scheme: 'file',
-          language: 'typescript',
-        },
-      ],
-      synchronize: {
-        fileEvents: workspace.createFileSystemWatcher('**/*.{ts,hbs}'),
-      },
-    };
-
-    const client = new LanguageClient('glint', 'Glint', serverOptions, clientOptions);
-
-    context.subscriptions.push(client.start());
+    workspace.workspaceFolders?.forEach((folder) => addWorkspaceFolder(folder, context));
+    workspace.onDidChangeWorkspaceFolders(({ added, removed }) => {
+      added.forEach((folder) => addWorkspaceFolder(folder, context));
+      removed.forEach((folder) => removeWorkspaceFolder(folder, context));
+    });
   },
 };
+
+const outputChannel = window.createOutputChannel('Glint Language Server');
+const clients = new Map<string, Disposable>();
+
+function addWorkspaceFolder(workspaceFolder: WorkspaceFolder, context: ExtensionContext): void {
+  let folderPath = workspaceFolder.uri.fsPath;
+  if (clients.has(folderPath)) return;
+
+  let executable = {
+    command: resolve('@glint/core/bin/glint-language-server', { basedir: folderPath }),
+  };
+
+  let serverOptions: ServerOptions = {
+    run: executable,
+    debug: executable,
+  };
+
+  let clientOptions: LanguageClientOptions = {
+    workspaceFolder,
+    outputChannel,
+    documentSelector: [
+      {
+        scheme: 'file',
+        language: 'handlebars',
+        pattern: `${folderPath}/**/*`,
+      },
+      {
+        scheme: 'file',
+        language: 'typescript',
+        pattern: `${folderPath}/**/*`,
+      },
+    ],
+    synchronize: {
+      fileEvents: workspace.createFileSystemWatcher(`${folderPath}/**/*.{ts,hbs}`),
+    },
+  };
+
+  const client = new LanguageClient('glint', 'Glint', serverOptions, clientOptions);
+  const disposable = client.start();
+
+  context.subscriptions.push(disposable);
+  clients.set(folderPath, disposable);
+}
+
+function removeWorkspaceFolder(workspaceFolder: WorkspaceFolder, context: ExtensionContext): void {
+  let folderPath = workspaceFolder.uri.fsPath;
+  let client = clients.get(folderPath);
+  if (client) {
+    clients.delete(folderPath);
+    context.subscriptions.splice(context.subscriptions.indexOf(client), 1);
+    client.dispose();
+  }
+}
