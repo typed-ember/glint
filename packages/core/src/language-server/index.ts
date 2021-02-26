860aa60ec3318e729868c0cab775845ff3351098
Dan Freeman <dfreeman@salsify.com>
2021-02-26 17:23:34 +0100

    Don't crash the language-server if no glint config is found

--- packages/core/src/language-server/index.ts
+++ packages/core/src/language-server/index.ts
@@ -1,6 +1,6 @@
 import { TextDocuments, TextDocumentSyncKind, createConnection } from 'vscode-languageserver/node';
 import { TextDocument } from 'vscode-languageserver-textdocument';
-import { loadConfig } from '@glint/config';
+import { findConfig } from '@glint/config';
 import { loadTypeScript } from '../common/load-typescript';
 import GlintLanguageServer from './glint-language-server';
 import { parseConfigFile, uriToFilePath } from './util';
@@ -10,7 +10,7 @@ const connection = createConnection(process.stdin, process.stdout);
 const documents = new TextDocuments(TextDocument);
 
 const ts = loadTypeScript();
-const glintConfig = loadConfig(process.cwd());
+const glintConfig = findConfig(process.cwd());
 const tsconfigPath = ts.findConfigFile(process.cwd(), ts.sys.fileExists);
 const { fileNames, options } = parseConfigFile(ts, tsconfigPath);
 const tsFileNames = fileNames.filter((fileName) => /\.ts$/.test(fileName));
@@ -18,85 +18,89 @@ const getRootFileNames = (): Array<string> => {
   return tsFileNames.concat(documents.all().map((doc) => uriToFilePath(doc.uri)));
 };
 
-const gls = new GlintLanguageServer(ts, glintConfig, getRootFileNames, options);
+if (glintConfig) {
+  const gls = new GlintLanguageServer(ts, glintConfig, getRootFileNames, options);
 
-connection.onInitialize(() => ({
-  capabilities: {
-    textDocumentSync: TextDocumentSyncKind.Full,
-    completionProvider: {
-      resolveProvider: true,
+  connection.onInitialize(() => ({
+    capabilities: {
+      textDocumentSync: TextDocumentSyncKind.Full,
+      completionProvider: {
+        resolveProvider: true,
+      },
+      referencesProvider: true,
+      hoverProvider: true,
+      definitionProvider: true,
+      workspaceSymbolProvider: true,
+      renameProvider: {
+        prepareProvider: true,
+      },
     },
-    referencesProvider: true,
-    hoverProvider: true,
-    definitionProvider: true,
-    workspaceSymbolProvider: true,
-    renameProvider: {
-      prepareProvider: true,
-    },
-  },
-}));
+  }));
 
-const scheduleDiagnostics = debounce(250, () => {
-  for (let { uri } of documents.all()) {
-    connection.sendDiagnostics({
-      uri,
-      diagnostics: gls.getDiagnostics(uri),
-    });
-  }
-});
+  const scheduleDiagnostics = debounce(250, () => {
+    for (let { uri } of documents.all()) {
+      connection.sendDiagnostics({
+        uri,
+        diagnostics: gls.getDiagnostics(uri),
+      });
+    }
+  });
 
-connection.onDidChangeWatchedFiles(() => {
-  // TODO: use this to synchronize files that aren't open so we don't assume changes only
-  // happen in the editor.
-});
+  connection.onDidChangeWatchedFiles(() => {
+    // TODO: use this to synchronize files that aren't open so we don't assume changes only
+    // happen in the editor.
+  });
 
-documents.onDidOpen(({ document }) => {
-  gls.openFile(document.uri, document.getText());
+  documents.onDidOpen(({ document }) => {
+    gls.openFile(document.uri, document.getText());
 
-  scheduleDiagnostics();
-});
+    scheduleDiagnostics();
+  });
 
-documents.onDidClose(({ document }) => {
-  gls.closeFile(document.uri);
-});
+  documents.onDidClose(({ document }) => {
+    gls.closeFile(document.uri);
+  });
 
-documents.onDidChangeContent(({ document }) => {
-  gls.updateFile(document.uri, document.getText());
+  documents.onDidChangeContent(({ document }) => {
+    gls.updateFile(document.uri, document.getText());
 
-  scheduleDiagnostics();
-});
+    scheduleDiagnostics();
+  });
 
-connection.onPrepareRename(({ textDocument, position }) => {
-  return gls.prepareRename(textDocument.uri, position);
-});
+  connection.onPrepareRename(({ textDocument, position }) => {
+    return gls.prepareRename(textDocument.uri, position);
+  });
 
-connection.onRenameRequest(({ textDocument, position, newName }) => {
-  return gls.getEditsForRename(textDocument.uri, position, newName);
-});
+  connection.onRenameRequest(({ textDocument, position, newName }) => {
+    return gls.getEditsForRename(textDocument.uri, position, newName);
+  });
 
-connection.onCompletion(({ textDocument, position }) => {
-  return gls.getCompletions(textDocument.uri, position);
-});
+  connection.onCompletion(({ textDocument, position }) => {
+    return gls.getCompletions(textDocument.uri, position);
+  });
 
-connection.onCompletionResolve((item) => {
-  return gls.getCompletionDetails(item);
-});
+  connection.onCompletionResolve((item) => {
+    return gls.getCompletionDetails(item);
+  });
 
-connection.onHover(({ textDocument, position }) => {
-  return gls.getHover(textDocument.uri, position);
-});
+  connection.onHover(({ textDocument, position }) => {
+    return gls.getHover(textDocument.uri, position);
+  });
 
-connection.onDefinition(({ textDocument, position }) => {
-  return gls.getDefinition(textDocument.uri, position);
-});
+  connection.onDefinition(({ textDocument, position }) => {
+    return gls.getDefinition(textDocument.uri, position);
+  });
 
-connection.onReferences(({ textDocument, position }) => {
-  return gls.getReferences(textDocument.uri, position);
-});
+  connection.onReferences(({ textDocument, position }) => {
+    return gls.getReferences(textDocument.uri, position);
+  });
 
-connection.onWorkspaceSymbol(({ query }) => {
-  return gls.findSymbols(query);
-});
+  connection.onWorkspaceSymbol(({ query }) => {
+    return gls.findSymbols(query);
+  });
 
-documents.listen(connection);
-connection.listen();
+  documents.listen(connection);
+  connection.listen();
+} else {
+  connection.console.info(`No Glint config found from ${process.cwd()}`);
+}
