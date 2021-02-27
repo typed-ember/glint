328515540674e3ebed680c656777c8f3289fce3a
Dan Freeman <dfreeman@salsify.com>
2021-02-27 16:09:40 +0100

    Fix VSCode tests when Code is already running

--- packages/vscode/__tests__/support/launch-from-cli.js
+++ packages/vscode/__tests__/support/launch-from-cli.js
@@ -1,14 +1,22 @@
 // @ts-check
 
 const path = require('path');
+const os = require('os');
 const { runTests } = require('vscode-test');
 
 async function main() {
+  let dataDir = path.join(os.tmpdir(), `user-data-${Math.random()}`);
+
   try {
     await runTests({
       extensionDevelopmentPath: path.resolve(__dirname, '../..'),
       extensionTestsPath: path.resolve(__dirname, 'vscode-runner.js'),
-      launchArgs: ['--disable-extensions', `${__dirname}/../__fixtures__/ember-app`],
+      launchArgs: [
+        '--disable-extensions',
+        '--user-data-dir',
+        dataDir,
+        `${__dirname}/../../__fixtures__/ember-app`,
+      ],
     });
   } catch (error) {
     console.error('Failed to launch tests:', error);
