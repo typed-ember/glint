184f782fc4394df12e716dadf61de1f0ba25111e
Dan Freeman <dfreeman@salsify.com>
2021-03-02 15:22:16 +0100

    Pull out LSP binding logic to a separate module

---
+++ packages/core/src/language-server/binding.ts
@@ -0,0 +1,113 @@
+import {
+  Connection,
+  ServerCapabilities,
+  TextDocuments,
+  TextDocumentSyncKind,
+} from 'vscode-languageserver';
+import { TextDocument } from 'vscode-languageserver-textdocument';
+import GlintLanguageServer from './glint-language-server';
+import { debounce } from '../common/scheduling';
+
+export const capabilities: ServerCapabilities = {
+  textDocumentSync: TextDocumentSyncKind.Full,
+  completionProvider: {
+    resolveProvider: true,
+  },
+  referencesProvider: true,
+  hoverProvider: true,
+  definitionProvider: true,
+  workspaceSymbolProvider: true,
+  renameProvider: {
+    prepareProvider: true,
+  },
+};
+
+export type BindingArgs = {
+  languageServer: GlintLanguageServer;
+  documents: TextDocuments<TextDocument>;
+  connection: Connection;
+};
+
+export function bindLanguageServer(args: BindingArgs): void {
+  let { connection, languageServer, documents } = args;
+  let { scheduleDiagnostics, captureErrors } = buildHelpers(args);
+
+  connection.onInitialize(() => ({ capabilities }));
+
+  documents.onDidOpen(({ document }) => {
+    languageServer.openFile(document.uri, document.getText());
+    scheduleDiagnostics();
+  });
+
+  documents.onDidClose(({ document }) => {
+    languageServer.closeFile(document.uri);
+  });
+
+  documents.onDidChangeContent(({ document }) => {
+    languageServer.updateFile(document.uri, document.getText());
+    scheduleDiagnostics();
+  });
+
+  connection.onPrepareRename(({ textDocument, position }) => {
+    return captureErrors(() => languageServer.prepareRename(textDocument.uri, position));
+  });
+
+  connection.onRenameRequest(({ textDocument, position, newName }) => {
+    return captureErrors(() =>
+      languageServer.getEditsForRename(textDocument.uri, position, newName)
+    );
+  });
+
+  connection.onCompletion(({ textDocument, position }) => {
+    return captureErrors(() => languageServer.getCompletions(textDocument.uri, position));
+  });
+
+  connection.onCompletionResolve((item) => {
+    return captureErrors(() => languageServer.getCompletionDetails(item)) ?? item;
+  });
+
+  connection.onHover(({ textDocument, position }) => {
+    return captureErrors(() => languageServer.getHover(textDocument.uri, position));
+  });
+
+  connection.onDefinition(({ textDocument, position }) => {
+    return captureErrors(() => languageServer.getDefinition(textDocument.uri, position));
+  });
+
+  connection.onReferences(({ textDocument, position }) => {
+    return captureErrors(() => languageServer.getReferences(textDocument.uri, position));
+  });
+
+  connection.onWorkspaceSymbol(({ query }) => {
+    return captureErrors(() => languageServer.findSymbols(query));
+  });
+
+  connection.onDidChangeWatchedFiles(() => {
+    // TODO: use this to synchronize files that aren't open so we don't assume changes only
+    // happen in the editor.
+  });
+}
+
+type BindingHelpers = {
+  scheduleDiagnostics: () => void;
+  captureErrors: <T>(callback: () => T) => T | undefined;
+};
+
+function buildHelpers({ languageServer, documents, connection }: BindingArgs): BindingHelpers {
+  return {
+    scheduleDiagnostics: debounce(250, () => {
+      for (let { uri } of documents.all()) {
+        const diagnostics = languageServer.getDiagnostics(uri);
+        connection.sendDiagnostics({ uri, diagnostics });
+      }
+    }),
+
+    captureErrors(callback) {
+      try {
+        return callback();
+      } catch (error) {
+        connection.console.error(error.stack ?? error);
+      }
+    },
+  };
+}
--- packages/core/src/language-server/index.ts
+++ packages/core/src/language-server/index.ts
@@ -1,10 +1,10 @@
-import { TextDocuments, TextDocumentSyncKind, createConnection } from 'vscode-languageserver/node';
+import { TextDocuments, createConnection } from 'vscode-languageserver/node';
 import { TextDocument } from 'vscode-languageserver-textdocument';
 import { findConfig } from '@glint/config';
 import { loadTypeScript } from '../common/load-typescript';
 import GlintLanguageServer from './glint-language-server';
 import { parseConfigFile, uriToFilePath } from './util';
-import { debounce } from '../common/scheduling';
+import { bindLanguageServer } from './binding';
 
 const connection = createConnection(process.stdin, process.stdout);
 const documents = new TextDocuments(TextDocument);
@@ -18,94 +18,10 @@ const getRootFileNames = (): Array<string> => {
   return tsFileNames.concat(documents.all().map((doc) => uriToFilePath(doc.uri)));
 };
 
-function captureErrors<T>(callback: () => T): T | undefined {
-  try {
-    return callback();
-  } catch (error) {
-    connection.console.error(error.stack ?? error);
-  }
-}
-
 if (glintConfig) {
-  const gls = new GlintLanguageServer(ts, glintConfig, getRootFileNames, options);
+  const languageServer = new GlintLanguageServer(ts, glintConfig, getRootFileNames, options);
 
-  connection.onInitialize(() => ({
-    capabilities: {
-      textDocumentSync: TextDocumentSyncKind.Full,
-      completionProvider: {
-        resolveProvider: true,
-      },
-      referencesProvider: true,
-      hoverProvider: true,
-      definitionProvider: true,
-      workspaceSymbolProvider: true,
-      renameProvider: {
-        prepareProvider: true,
-      },
-    },
-  }));
-
-  const scheduleDiagnostics = debounce(250, () => {
-    for (let { uri } of documents.all()) {
-      connection.sendDiagnostics({
-        uri,
-        diagnostics: gls.getDiagnostics(uri),
-      });
-    }
-  });
-
-  connection.onDidChangeWatchedFiles(() => {
-    // TODO: use this to synchronize files that aren't open so we don't assume changes only
-    // happen in the editor.
-  });
-
-  documents.onDidOpen(({ document }) => {
-    gls.openFile(document.uri, document.getText());
-
-    scheduleDiagnostics();
-  });
-
-  documents.onDidClose(({ document }) => {
-    gls.closeFile(document.uri);
-  });
-
-  documents.onDidChangeContent(({ document }) => {
-    gls.updateFile(document.uri, document.getText());
-
-    scheduleDiagnostics();
-  });
-
-  connection.onPrepareRename(({ textDocument, position }) => {
-    return captureErrors(() => gls.prepareRename(textDocument.uri, position));
-  });
-
-  connection.onRenameRequest(({ textDocument, position, newName }) => {
-    return captureErrors(() => gls.getEditsForRename(textDocument.uri, position, newName));
-  });
-
-  connection.onCompletion(({ textDocument, position }) => {
-    return captureErrors(() => gls.getCompletions(textDocument.uri, position));
-  });
-
-  connection.onCompletionResolve((item) => {
-    return captureErrors(() => gls.getCompletionDetails(item)) ?? item;
-  });
-
-  connection.onHover(({ textDocument, position }) => {
-    return captureErrors(() => gls.getHover(textDocument.uri, position));
-  });
-
-  connection.onDefinition(({ textDocument, position }) => {
-    return captureErrors(() => gls.getDefinition(textDocument.uri, position));
-  });
-
-  connection.onReferences(({ textDocument, position }) => {
-    return captureErrors(() => gls.getReferences(textDocument.uri, position));
-  });
-
-  connection.onWorkspaceSymbol(({ query }) => {
-    return captureErrors(() => gls.findSymbols(query));
-  });
+  bindLanguageServer({ languageServer, documents, connection });
 
   documents.listen(connection);
   connection.listen();
