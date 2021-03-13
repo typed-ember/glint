4294263268900dabb0a10d146c0fa5e0bc851b42
Dan Freeman <dfreeman@salsify.com>
2021-03-13 17:14:28 +0100

    Add new global entries and brief doc comments for Ember intrinsics

--- packages/environment-ember-loose/types/intrinsics/log.d.ts
+++ packages/environment-ember-loose/types/intrinsics/log.d.ts
@@ -1,6 +1,6 @@
 import { NoNamedArgs } from '@glint/template/-private';
 import { DirectInvokable } from '@glint/template/-private/resolution';
 
-export type LogKeyword = DirectInvokable<{
+export type LogHelper = DirectInvokable<{
   (args: NoNamedArgs, ...params: unknown[]): void;
 }>;
