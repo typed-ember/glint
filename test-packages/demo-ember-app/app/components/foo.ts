c5ad96b46c1be3943fd8518ee2d053637f63f69d
Dan Freeman <dfreeman@salsify.com>
2021-03-17 09:36:20 +0100

    Use a decentralized registry for `environment-ember-loose`

--- test-packages/demo-ember-app/app/components/foo.ts
+++ test-packages/demo-ember-app/app/components/foo.ts
@@ -5,3 +5,9 @@ export default class Foo extends Component {
 
   obj = { a: 'A', b: 'B', c: 1, 𝚪: '' };
 }
+
+declare module '@glint/environment-ember-loose/types/registry' {
+  export default interface Registry {
+    Foo: typeof Foo;
+  }
+}
--- test-packages/demo-ember-app/app/components/qux.ts
+++ test-packages/demo-ember-app/app/components/qux.ts
@@ -3,3 +3,9 @@ import Component from '@glint/environment-ember-loose/glimmer-component';
 export default class Qux extends Component {
   name = 'QUX';
 }
+
+declare module '@glint/environment-ember-loose/types/registry' {
+  export default interface Registry {
+    Qux: typeof Qux;
+  }
+}
