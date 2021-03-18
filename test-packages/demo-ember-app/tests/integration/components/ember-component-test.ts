e97aa068ef058ff48600fdbd3b00f51260936be5
James C. Davis <jdavis@salsify.com>
2021-03-18 12:05:54 -0400

    Add tests for EmberComponent

---
+++ test-packages/demo-ember-app/tests/integration/components/ember-component-test.ts
@@ -0,0 +1,44 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | ember', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    await render(hbs`<EmberComponent @required="foo" />`);
+
+    if (!this.element.textContent) {
+      throw new Error('No text content!');
+    }
+
+    assert.deepEqual(
+      this.element.textContent.trim().split(/\s+/),
+      ['foo','defaultValue','foo']
+    );
+  });
+
+  test('it renders with incorrect arg type', async function(assert) {
+    await render(hbs`<EmberComponent @required=1 />`);
+
+    if (!this.element.textContent) {
+      throw new Error('No text content!');
+    }
+
+    assert.deepEqual(
+      this.element.textContent.trim().split(/\s+/),
+      ['1','defaultValue','1']
+    );
+  });
+
+  test('it renders with missing args', async function(assert) {
+    await render(hbs`<EmberComponent />`);
+
+    if (!this.element.textContent) {
+      throw new Error('No text content!');
+    }
+
+    assert.equal(this.element.textContent.trim(), 'defaultValue');
+  });
+});
