66a98478992e82976ca5383a66f2bc834106d3e0
Peter Wagenet <peter.wagenet@gmail.com>
2021-03-30 16:55:41 -0700

    Support inverse for {{#each-in}} - Fixes #108

--- packages/environment-ember-loose/__tests__/intrinsics/each-in.test.ts
+++ packages/environment-ember-loose/__tests__/intrinsics/each-in.test.ts
@@ -9,3 +9,56 @@ invokeBlock(eachIn({}, { a: 5, b: 3 }), {
     expectTypeOf(value).toEqualTypeOf<number>();
   },
 });
+
+// Can render maybe undefined
+
+declare const maybeVal: { a: number; b: number } | undefined;
+
+invokeBlock(eachIn({}, maybeVal), {
+  default(key, value) {
+    expectTypeOf(key).toEqualTypeOf<'a' | 'b'>();
+    expectTypeOf(value).toEqualTypeOf<number>();
+  },
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  inverse(..._args: never) {
+    // This block receives no args
+  },
+});
+
+// Can render inverse when undefined, null, or empty.
+
+invokeBlock(eachIn({}, undefined), {
+  default(key, value) {
+    // This won't get called when no value, but gets default for keyof
+    expectTypeOf(key).toEqualTypeOf<string | number | symbol>();
+    expectTypeOf(value).toEqualTypeOf<never>();
+  },
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  inverse(..._args: never) {
+    // This block receives no args
+  },
+});
+
+invokeBlock(eachIn({}, null), {
+  default(key, value) {
+    // This won't get called when no value, but gets default for keyof
+    expectTypeOf(key).toEqualTypeOf<string | number | symbol>();
+    expectTypeOf(value).toEqualTypeOf<never>();
+  },
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  inverse(..._args: never) {
+    // This block receives no args
+  },
+});
+
+invokeBlock(eachIn({}, {}), {
+  default(key, value) {
+    // This won't get called when no value
+    expectTypeOf(key).toEqualTypeOf<never>();
+    expectTypeOf(value).toEqualTypeOf<never>();
+  },
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  inverse(..._args: never) {
+    // This block receives no args
+  },
+});
