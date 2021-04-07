628ff84968ab9f66fcc3dfa2e679b60faa255f4a
Dan Freeman <dfreeman@salsify.com>
2021-04-07 14:07:56 +0200

    Emit attrs and modifiers within the containing element/component

--- packages/transform/src/template-to-typescript.ts
+++ packages/transform/src/template-to-typescript.ts
@@ -268,26 +268,13 @@ export function templateToTypescript(
       }
     }
 
-    type ElementNodeType = 'component' | 'element';
-
     function emitElementNode(node: AST.ElementNode): void {
       let firstCharacter = node.tag.charAt(0);
-      let nodeType: ElementNodeType =
-        firstCharacter.toUpperCase() === firstCharacter || node.tag.includes('.')
-          ? 'component'
-          : 'element';
-
-      emit.forNode(node, () => {
-        emitPlainAttributes(node, nodeType);
-        emitSplattributes(node, nodeType);
-        emitModifiers(node, nodeType);
-
-        if (nodeType === 'component') {
-          emitComponent(node);
-        } else {
-          emitPlainElement(node);
-        }
-      });
+      if (firstCharacter.toUpperCase() === firstCharacter || node.tag.includes('.')) {
+        emitComponent(node);
+      } else {
+        emitPlainElement(node);
+      }
     }
 
     function emitConcatStatement(node: AST.ConcatStatement): void {
@@ -386,11 +373,14 @@ export function templateToTypescript(
         emit.indent();
         emit.newline();
 
+        emitSplattributes(node);
+        emitPlainAttributes(node);
+        emitModifiers(node);
+
         emit.text('χ.bindBlocks(𝛄.blockParams, {');
 
         if (node.selfClosing) {
           emit.text('});');
-          emit.newline();
         } else {
           emit.newline();
           emit.indent();
@@ -431,6 +421,7 @@ export function templateToTypescript(
           emit.text(';');
         }
 
+        emit.newline();
         emit.dedent();
         emit.text('});');
         emit.newline();
@@ -499,38 +490,35 @@ export function templateToTypescript(
     }
 
     function emitPlainElement(node: AST.ElementNode): void {
-      for (let child of node.children) {
-        emitTopLevelStatement(child);
-      }
-    }
-
-    function emitElementTypeReference(node: AST.ElementNode, type: ElementNodeType): void {
-      let { start, path, kind } = tagNameToPathContents(node);
-
-      emit.text('import("');
-      emit.text(typesPath);
-      emit.text('").');
-      if (type === 'component') {
-        emit.text('ElementForComponent<typeof ');
-        emitPathContents(path, start, kind);
-        emit.text('>');
-      } else {
-        emit.text('ElementForTagName<');
+      emit.forNode(node, () => {
+        emit.text('χ.emitElement(');
         emit.text(JSON.stringify(node.tag));
-        emit.text('>');
-      }
+        emit.text(', 𝛄 => {');
+        emit.newline();
+        emit.indent();
+
+        emitSplattributes(node);
+        emitPlainAttributes(node);
+        emitModifiers(node);
+
+        for (let child of node.children) {
+          emitTopLevelStatement(child);
+        }
+
+        emit.dedent();
+        emit.text('});');
+        emit.newline();
+      });
     }
 
-    function emitPlainAttributes(node: AST.ElementNode, nodeType: ElementNodeType): void {
+    function emitPlainAttributes(node: AST.ElementNode): void {
       let attributes = node.attributes.filter(
         (attr) => !attr.name.startsWith('@') && attr.name !== SPLATTRIBUTES
       );
 
       if (!attributes.length) return;
 
-      emit.text('χ.applyAttributes<');
-      emitElementTypeReference(node, nodeType);
-      emit.text('>({');
+      emit.text('χ.applyAttributes(𝛄.element, {');
       emit.newline();
       emit.indent();
 
@@ -561,7 +549,7 @@ export function templateToTypescript(
       emit.newline();
     }
 
-    function emitSplattributes(node: AST.ElementNode, nodeType: ElementNodeType): void {
+    function emitSplattributes(node: AST.ElementNode): void {
       let splattributes = node.attributes.find((attr) => attr.name === SPLATTRIBUTES);
       if (!splattributes) return;
 
@@ -571,19 +559,16 @@ export function templateToTypescript(
       );
 
       emit.forNode(splattributes, () => {
-        emit.text('χ.applySplattributes<typeof 𝚪.element, ');
-        emitElementTypeReference(node, nodeType);
-        emit.text('>();');
-        emit.newline();
+        emit.text('χ.applySplattributes(𝚪.element, 𝛄.element);');
       });
+
+      emit.newline();
     }
 
-    function emitModifiers(node: AST.ElementNode, nodeType: ElementNodeType): void {
+    function emitModifiers(node: AST.ElementNode): void {
       for (let modifier of node.modifiers) {
         emit.forNode(modifier, () => {
-          emit.text('χ.applyModifier<');
-          emitElementTypeReference(node, nodeType);
-          emit.text('>(');
+          emit.text('χ.applyModifier(𝛄.element, ');
           emitResolve(modifier, 'resolve');
           emit.text(');');
           emit.newline();
