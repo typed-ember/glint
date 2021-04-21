209a724f422e7823d6b234294f7c6b3f78967c93
Dan Freeman <dfreeman@salsify.com>
2021-04-21 17:17:56 -0600

    Add classic and pod smoketest components

---
+++ packages/vscode/__fixtures__/ember-app/app/components/classic-layout.ts
@@ -0,0 +1,5 @@
+import Component from '@glint/environment-ember-loose/glimmer-component';
+
+export default class ClassicLayoutComponent extends Component {
+  private message = 'hello';
+}
--- packages/vscode/__tests__/smoketest-ember.test.ts
+++ packages/vscode/__tests__/smoketest-ember.test.ts
@@ -1,4 +1,14 @@
-import { commands, languages, Position, ViewColumn, window, Uri, Range } from 'vscode';
+import {
+  commands,
+  languages,
+  Position,
+  ViewColumn,
+  window,
+  Uri,
+  Range,
+  extensions,
+  Location,
+} from 'vscode';
 import path from 'path';
 import { waitUntil } from './helpers/async';
 
@@ -8,7 +18,10 @@ describe('Smoke test: Ember', () => {
   const rootDir = path.resolve(__dirname, '../__fixtures__/ember-app');
 
   afterEach(async () => {
-    await commands.executeCommand('workbench.action.closeAllEditors');
+    while (window.activeTextEditor) {
+      await commands.executeCommand('workbench.action.files.revert');
+      await commands.executeCommand('workbench.action.closeActiveEditor');
+    }
   });
 
   test('introducing an error', async () => {
@@ -40,5 +53,64 @@ describe('Smoke test: Ember', () => {
         range: new Range(0, 7, 0, 14),
       },
     ]);
+  });
+
+  describe('component layout', () => {
+    test('classic', async () => {
+      let scriptURI = Uri.file(`${rootDir}/app/components/classic-layout.ts`);
+      let templateURI = Uri.file(`${rootDir}/app/templates/components/classic-layout.hbs`);
+
+      await window.showTextDocument(templateURI);
+
+      await waitUntil(() => extensions.getExtension('typed-ember.glint-vscode')?.isActive);
+
+      let positions = (await commands.executeCommand(
+        'vscode.executeDefinitionProvider',
+        templateURI,
+        new Position(0, 11)
+      )) as Array<Location>;
+
+      expect(positions.length).toBe(1);
+      expect(positions[0].uri.fsPath).toEqual(scriptURI.fsPath);
+      expect(positions[0].range).toEqual(new Range(3, 10, 3, 17));
+    });
+
+    test('colocated', async () => {
+      let scriptURI = Uri.file(`${rootDir}/app/components/colocated-layout.ts`);
+      let templateURI = Uri.file(`${rootDir}/app/components/colocated-layout.hbs`);
+
+      await window.showTextDocument(templateURI);
+
+      await waitUntil(() => extensions.getExtension('typed-ember.glint-vscode')?.isActive);
+
+      let positions = (await commands.executeCommand(
+        'vscode.executeDefinitionProvider',
+        templateURI,
+        new Position(0, 11)
+      )) as Array<Location>;
+
+      expect(positions.length).toBe(1);
+      expect(positions[0].uri.fsPath).toEqual(scriptURI.fsPath);
+      expect(positions[0].range).toEqual(new Range(3, 10, 3, 17));
+    });
+
+    test('pod', async () => {
+      let scriptURI = Uri.file(`${rootDir}/app/components/pod-layout/component.ts`);
+      let templateURI = Uri.file(`${rootDir}/app/components/pod-layout/template.hbs`);
+
+      await window.showTextDocument(templateURI);
+
+      await waitUntil(() => extensions.getExtension('typed-ember.glint-vscode')?.isActive);
+
+      let positions = (await commands.executeCommand(
+        'vscode.executeDefinitionProvider',
+        templateURI,
+        new Position(0, 11)
+      )) as Array<Location>;
+
+      expect(positions.length).toBe(1);
+      expect(positions[0].uri.fsPath).toEqual(scriptURI.fsPath);
+      expect(positions[0].range).toEqual(new Range(3, 10, 3, 17));
+    });
   });
 });
