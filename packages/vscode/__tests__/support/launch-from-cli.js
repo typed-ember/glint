e582da0893a6e4858388a89127b3010d99943b03
Dan Freeman <dfreeman@salsify.com>
2021-11-15 17:50:44 +0100

    Ensure the built-in TS extension doesn't mess with our smoke tests

--- packages/vscode/__tests__/support/launch-from-cli.js
+++ packages/vscode/__tests__/support/launch-from-cli.js
@@ -5,17 +5,25 @@ const os = require('os');
 const { runTests } = require('vscode-test');
 
 async function main() {
-  let dataDir = path.join(os.tmpdir(), `user-data-${Math.random()}`);
+  let emptyTempDir = path.join(os.tmpdir(), `user-data-${Math.random()}`);
 
   try {
     await runTests({
       extensionDevelopmentPath: path.resolve(__dirname, '../..'),
       extensionTestsPath: path.resolve(__dirname, 'vscode-runner.js'),
       launchArgs: [
-        '--disable-extensions',
+        // Don't show the "hey do you trust this folder?" prompt
         '--disable-workspace-trust',
+        // Explicitly turn off the built-in TS extension
+        '--disable-extension',
+        'vscode.typescript-language-features',
+        // Point at an empty directory so no third-party extensions load
+        '--extensions-dir',
+        emptyTempDir,
+        // Point at an empty directory so we don't have to contend with any local user preferences
         '--user-data-dir',
-        dataDir,
+        emptyTempDir,
+        // Load the Ember and GlimmerX app fixtures
         `${__dirname}/../../__fixtures__/ember-app`,
         `${__dirname}/../../__fixtures__/js-glimmerx-app`,
       ],
