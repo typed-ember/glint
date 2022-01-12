06aaf876bcd73607c3cf8dee084a483d33a1916b
Dan Freeman <dfreeman@salsify.com>
2022-01-12 18:15:00 +0100

    Silence Browserslist warnings in the CLI and LS

--- packages/core/bin/glint-language-server.js
+++ packages/core/bin/glint-language-server.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 /* eslint-disable */
+
+// Despite the fact that we're only using Babel for parsing,
+// it implicitly loads Browserslist, which can trigger a
+// warning to stdout (breaking our LSP interactions) for
+// users with an older lockfile.
+process.env.BROWSERSLIST_IGNORE_OLD_DATA = 'true';
+
 require('../lib/language-server');
--- packages/core/bin/glint.js
+++ packages/core/bin/glint.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 /* eslint-disable */
+
+// Despite the fact that we're only using Babel for parsing,
+// it implicitly loads Browserslist, which can trigger a
+// warning for users with an older lockfile.
+process.env.BROWSERSLIST_IGNORE_OLD_DATA = 'true';
+
 require('../lib/cli');
