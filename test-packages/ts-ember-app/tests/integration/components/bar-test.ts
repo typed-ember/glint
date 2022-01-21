e089c26f11360661d007dc215b2986f5294b806c
Dan Freeman <dfreeman@salsify.com>
2022-01-21 22:20:31 +0100

    Fix lingering ember-test-helpers references

--- test-packages/ts-ember-app/tests/integration/components/bar-test.ts
+++ test-packages/ts-ember-app/tests/integration/components/bar-test.ts
@@ -2,7 +2,7 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
-import type { TestContext } from 'ember-test-helpers';
+import type { TestContext } from '@ember/test-helpers';
 
 module('Integration | Component | bar', function (hooks) {
   setupRenderingTest(hooks);
