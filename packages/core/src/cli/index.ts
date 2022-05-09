2fcbe71421967f57f6a200ac7668e0fe561380d6
Chris Krycho <hello@chriskrycho.com>
2022-05-09 07:35:05 -0600

    core/cli: add `--build` and related commands
    Introduce new flags in the CLI, along with `--build` itself:
    
    * `--dry`
    * `--force`
    * `--incremental`
    * `--clean`
    * `projects`
    
    These options map directly to the `tsc` options, and allow users to
    actually *use* the newly-implemented build functionality.
    
    Introduce the check for running a `--build` *before* falling into the
    default case, so that `--build` and especially `--build --watch` has
    the right behavior.
    
    This is straightforward *except* for the hacky use of a default command
    to capture the optional positional arguments allowed by `tsc` with its
    `--build` options. That pattern for `yargs` is documented [here][1], so
    is considered supported even if a bit odd.
    
    [1]: https://github.com/yargs/yargs/blob/main/docs/advanced.md#default-commands

--- packages/core/src/cli/index.ts
+++ packages/core/src/cli/index.ts
@@ -3,6 +3,9 @@ import { loadConfig } from '@glint/config';
 import { performWatch } from './perform-watch';
 import { performCheck } from './perform-check';
 import { determineOptionsToExtend } from './options';
+import { performBuild } from './perform-build';
+import type TS from 'typescript';
+import { performBuildWatch } from './perform-build-watch';
 
 const argv = yargs
   .scriptName('glint')
@@ -22,6 +25,44 @@ const argv = yargs
     boolean: true,
     description: 'Whether to emit declaration files',
   })
+  .option('build', {
+    alias: 'b',
+    boolean: true,
+    description:
+      'Build one or more projects and their dependencies, if out of date. Same as the TS `--build` flag.',
+    // As with TS itself, we *must* emit declarations when in build mode
+    conflicts: 'declaration',
+  })
+  .option('clean', {
+    implies: 'build',
+    boolean: true,
+    description: 'Delete the outputs of all projects.',
+    conflicts: 'watch',
+  })
+  .option('force', {
+    implies: 'build',
+    description: 'Act as if all projects are out of date. Same as the TS `--force` flag.',
+    type: 'boolean',
+  })
+  .option('dry', {
+    implies: 'build',
+    description: `Show what would be built (or deleted, if specified with '--clean'). Same as the TS \`--dry\` flag.`,
+    type: 'boolean',
+  })
+  .option('incremental', {
+    description:
+      'Save .tsbuildinfo files to allow for incremental compilation of projects. Same as the TS `--incremental` flag.',
+    type: 'boolean',
+  })
+  // Use a 'default command' as a hack to get top-level positional arguments.
+  // See https://github.com/yargs/yargs/blob/main/docs/advanced.md#default-commands
+  .command('$0', false, (commandYargs) => {
+    commandYargs.positional('projects', {
+      description: 'A list of projects to compile, when using --build',
+      type: 'string',
+      array: true,
+    });
+  })
   .option('debug-intermediate-representation', {
     boolean: false,
     description: `When true, writes out a Glint's internal intermediate representation of each file within a GLINT_DEBUG subdirectory of the current working directory. This is intended for debugging Glint itself.`,
@@ -31,7 +72,6 @@ const argv = yargs
   .parseSync();
 
 const glintConfig = loadConfig(argv.project ?? process.cwd());
-const optionsToExtend = determineOptionsToExtend(argv);
 
 if (argv['debug-intermediate-representation']) {
   const fs = require('fs');
@@ -43,6 +83,29 @@ if (argv['debug-intermediate-representation']) {
   };
 }
 
+if (argv.build) {
+  // Type signature here so we get a useful error as close to the source of the
+  // error as possible, rather than at the *use* sites below.
+  let buildOptions: TS.BuildOptions = {
+    clean: argv.clean,
+    force: argv.force,
+    dry: argv.dry,
+    incremental: argv.incremental,
+  };
+
+  // This continues using the hack of a 'default command' to get the projects
+  // specified (if any).
+  let projectsArg = argv._;
+  let projects = projectsArg.length > 0 ? projectsArg : ['.'];
+
+  if (argv.watch) {
+    performBuildWatch(glintConfig, projects, buildOptions);
+  } else {
+    performBuild(glintConfig, projects, buildOptions);
+  }
+}
+
+const optionsToExtend = determineOptionsToExtend(argv);
 if (argv.watch) {
   performWatch(glintConfig, optionsToExtend);
 } else {
