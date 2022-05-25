371e3f494e468ebfda6eec7e31cbaa6ea23f1f5f
Dan Freeman <dfreeman@salsify.com>
2022-05-25 16:54:44 +0200

    Add local tests for env-eti's preprocessing logic

---
+++ packages/environment-ember-template-imports/__tests__/transformation.test.ts
@@ -0,0 +1,170 @@
+import ts from 'typescript';
+import { stripIndent } from 'common-tags';
+
+import { preprocess } from '../-private/environment/preprocess';
+import { transform } from '../-private/environment/transform';
+import { GlintEmitMetadata } from '@glint/config/lib/environment';
+
+describe('Environment: ETI', () => {
+  describe('preprocess', () => {
+    test('single template', () => {
+      let source = '<template>hi</template>\n';
+      let transformed = '[___T`hi`]\n';
+      let result = preprocess(source, 'index.gts');
+
+      expect(result.contents).toEqual(transformed);
+      expect(result.data).toEqual({
+        templateLocations: [
+          {
+            startTagOffset: source.indexOf('<template>'),
+            startTagLength: '<template>'.length,
+            endTagOffset: source.indexOf('</template>'),
+            endTagLength: '</template>'.length,
+            transformedStart: transformed.indexOf('[___T'),
+            transformedEnd: transformed.indexOf(']') + 1,
+          },
+        ],
+      });
+    });
+
+    test('multiple templates', () => {
+      let source = stripIndent`
+        <template>
+          <Foo />
+        </template>
+
+        class Foo {
+          <template>Hello</template>
+        }
+      `;
+
+      let transformed = stripIndent`
+        [___T\`
+          <Foo />
+        \`]
+
+        class Foo {
+          [___T\`Hello\`]
+        }
+      `;
+
+      let sourceClassOffset = source.indexOf('class');
+      let transformedClassOffset = transformed.indexOf('class');
+
+      let result = preprocess(source, 'index.gts');
+
+      expect(result.contents).toEqual(transformed);
+      expect(result.data).toEqual({
+        templateLocations: [
+          {
+            startTagOffset: source.indexOf('<template>'),
+            startTagLength: '<template>'.length,
+            endTagOffset: source.indexOf('</template>'),
+            endTagLength: '</template>'.length,
+            transformedStart: transformed.indexOf('[___T'),
+            transformedEnd: transformed.indexOf(']') + 1,
+          },
+          {
+            startTagOffset: source.indexOf('<template>', sourceClassOffset),
+            startTagLength: '<template>'.length,
+            endTagOffset: source.indexOf('</template>', sourceClassOffset),
+            endTagLength: '</template>'.length,
+            transformedStart: transformed.indexOf('[___T', transformedClassOffset),
+            transformedEnd: transformed.indexOf(']', transformedClassOffset) + 1,
+          },
+        ],
+      });
+    });
+  });
+
+  describe('transform', () => {
+    function applyTransform(source: string): {
+      meta: Map<ts.Node, GlintEmitMetadata>;
+      sourceFile: ts.SourceFile;
+    } {
+      let meta = new Map<ts.Node, GlintEmitMetadata>();
+      let setEmitMetadata = (node: ts.TaggedTemplateExpression, newMeta: GlintEmitMetadata): void =>
+        void meta.set(node, Object.assign(meta.get(node) ?? {}, newMeta));
+
+      let { contents, data } = preprocess(source, 'index.gts');
+      let ast = ts.createSourceFile('index.gts', contents, ts.ScriptTarget.Latest, true);
+      let { transformed } = ts.transform(ast, [
+        (context) => transform(data!, { ts, context, setEmitMetadata }),
+      ]);
+
+      expect(transformed.length).toBe(1);
+      expect(transformed[0].kind).toBe(ts.SyntaxKind.SourceFile);
+
+      return { meta, sourceFile: transformed[0] as ts.SourceFile };
+    }
+
+    test('single template', () => {
+      let source = '<template>hi</template>\n';
+      let { meta, sourceFile } = applyTransform(source);
+      let templateNode = (sourceFile.statements[1] as ts.ExpressionStatement).expression;
+
+      expect(meta).toEqual(
+        new Map([
+          [
+            templateNode,
+            {
+              prepend: 'export default ',
+              templateLocation: {
+                start: source.indexOf('<template>'),
+                end: source.indexOf('</template>') + '</template>'.length,
+              },
+            },
+          ],
+        ])
+      );
+    });
+
+    test('multiple templates', () => {
+      let source = stripIndent`
+        <template>
+          <Foo />
+        </template>
+
+        class Foo {
+          <template>Hello</template>
+        }
+      `;
+
+      let classStart = source.indexOf('class');
+      let { meta, sourceFile } = applyTransform(source);
+      let firstTemplate = (sourceFile.statements[1] as ts.ExpressionStatement).expression;
+      let secondTemplate = (
+        (
+          (sourceFile.statements[2] as ts.ClassDeclaration)
+            .members[0] as ts.ClassStaticBlockDeclaration
+        ).body.statements[0] as ts.ExpressionStatement
+      ).expression;
+
+      expect(meta).toEqual(
+        new Map<ts.Node, GlintEmitMetadata>([
+          [
+            firstTemplate,
+            {
+              prepend: 'export default ',
+              templateLocation: {
+                start: source.indexOf('<template>'),
+                end: source.indexOf('</template>') + '</template>'.length,
+              },
+            },
+          ],
+          [
+            secondTemplate,
+            {
+              prepend: 'static { ',
+              append: ' }',
+              templateLocation: {
+                start: source.indexOf('<template>', classStart),
+                end: source.indexOf('</template>', classStart) + '</template>'.length,
+              },
+            },
+          ],
+        ])
+      );
+    });
+  });
+});
