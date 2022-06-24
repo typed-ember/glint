1c8a71a4e6832124e3407f31e8a3c4296305c520
Dan Freeman <dfreeman@salsify.com>
2022-06-24 20:15:37 +0200

    Update Code extension not to look for `.glintrc`

--- packages/vscode/src/extension.ts
+++ packages/vscode/src/extension.ts
@@ -6,7 +6,6 @@ import {
   ServerOptions,
 } from 'vscode-languageclient/node';
 import { sync as resolve } from 'resolve';
-import type { GlintConfig } from '@glint/config';
 
 module.exports = {
   activate(context: ExtensionContext) {
@@ -20,33 +19,19 @@ module.exports = {
 
 const outputChannel = window.createOutputChannel('Glint Language Server');
 const clients = new Map<string, Disposable>();
-let debugServerPortNumber = 6009;
 
 function addWorkspaceFolder(workspaceFolder: WorkspaceFolder, context: ExtensionContext): void {
   let folderPath = workspaceFolder.uri.fsPath;
   if (clients.has(folderPath)) return;
 
-  let executable = {
-    command: 'node',
-    args: [resolve('@glint/core/bin/glint-language-server', { basedir: folderPath })],
-  };
-
-  // Runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-  let debugExecutable = {
-    ...executable,
-    args: ['--nolazy', `--inspect=${debugServerPortNumber++}`, ...executable.args],
-  };
+  let nodeArgs = [resolve('@glint/core/bin/glint-language-server', { basedir: folderPath })];
 
   let serverOptions: ServerOptions = {
-    run: executable,
-    debug: debugExecutable,
+    run: { command: 'node', args: nodeArgs },
+    debug: { command: 'node', args: ['--nolazy', `--inspect`, ...nodeArgs] },
   };
 
-  let { loadConfig } = require(resolve('@glint/config', { basedir: folderPath }));
-  let config: GlintConfig = loadConfig(folderPath);
-
-  // Older versions of Glint won't have `getConfiguredFileExtensions`, so fallback to safe defaults.
-  let extensions = config.environment.getConfiguredFileExtensions?.() ?? ['.js', '.ts', '.hbs'];
+  let extensions = ['.js', '.ts', '.gjs', '.gts', '.hbs'];
   let filePattern = `${folderPath}/**/*{${extensions.join(',')}}`;
 
   let clientOptions: LanguageClientOptions = {
