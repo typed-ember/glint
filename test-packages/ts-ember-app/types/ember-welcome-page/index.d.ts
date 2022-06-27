5e409e1c9bff8c5a6604108a8d10baa8dfcf8f8b
Dan Freeman <dfreeman@salsify.com>
2022-06-27 17:52:33 +0200

    Upgrade `ts-ember-app` to Ember 4.4

--- test-packages/ts-ember-app/types/ember-welcome-page/index.d.ts
+++
@@ -1,9 +0,0 @@
-import Component from '@ember/component';
-
-declare class WelcomePage extends Component {}
-
-declare module '@glint/environment-ember-loose/registry' {
-  export default interface Registry {
-    WelcomePage: typeof WelcomePage;
-  }
-}
