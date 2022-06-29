bcd71aadc928b34dcad17a37c8fe9579b31e7422
Dan Freeman <dfreeman@salsify.com>
2022-06-29 17:51:11 +0200

    Upgrade language client and server libraries

--- packages/vscode/src/extension.ts
+++ packages/vscode/src/extension.ts
@@ -1,72 +1,79 @@
-import { ExtensionContext, workspace, window, WorkspaceFolder } from 'vscode';
 import {
-  Disposable,
-  LanguageClient,
-  LanguageClientOptions,
-  ServerOptions,
-} from 'vscode-languageclient/node';
+  ExtensionContext,
+  workspace,
+  window,
+  WorkspaceFolder,
+  FileSystemWatcher,
+  commands,
+} from 'vscode';
+import { LanguageClient, ServerOptions } from 'vscode-languageclient/node';
 import { sync as resolve } from 'resolve';
 
-module.exports = {
-  activate(context: ExtensionContext) {
-    workspace.workspaceFolders?.forEach((folder) => addWorkspaceFolder(folder, context));
-    workspace.onDidChangeWorkspaceFolders(({ added, removed }) => {
-      added.forEach((folder) => addWorkspaceFolder(folder, context));
-      removed.forEach((folder) => removeWorkspaceFolder(folder, context));
-    });
-  },
-};
-
 const outputChannel = window.createOutputChannel('Glint Language Server');
-const clients = new Map<string, Disposable>();
+const clients = new Map<string, LanguageClient>();
+const extensions = ['.js', '.ts', '.gjs', '.gts', '.hbs'];
+const filePattern = `**/*{${extensions.join(',')}}`;
 
-function addWorkspaceFolder(workspaceFolder: WorkspaceFolder, context: ExtensionContext): void {
+export function activate(context: ExtensionContext): void {
+  let watcher = workspace.createFileSystemWatcher(filePattern);
+
+  context.subscriptions.push(watcher);
+
+  workspace.workspaceFolders?.forEach((folder) => addWorkspaceFolder(folder, watcher));
+  workspace.onDidChangeWorkspaceFolders(({ added, removed }) => {
+    added.forEach((folder) => addWorkspaceFolder(folder, watcher));
+    removed.forEach((folder) => removeWorkspaceFolder(folder));
+  });
+}
+
+export async function deactivate(): Promise<void> {
+  await Promise.all([...clients.values()].map((client) => client.stop()));
+}
+
+
+async function addWorkspaceFolder(
+  workspaceFolder: WorkspaceFolder,
+  watcher: FileSystemWatcher
+): Promise<void> {
   let folderPath = workspaceFolder.uri.fsPath;
   if (clients.has(folderPath)) return;
 
-  let serverArgs = [];
+  let serverPath = findLanguageServer(folderPath);
+  if (!serverPath) return;
+
+  let serverOptions: ServerOptions = { module: serverPath };
+  let client = new LanguageClient('glint', 'Glint', serverOptions, {
+    workspaceFolder,
+    outputChannel,
+    documentSelector: [{ scheme: 'file', pattern: `${folderPath}/${filePattern}` }],
+    synchronize: { fileEvents: watcher },
+  });
+
+  clients.set(folderPath, client);
+
+  await client.start();
+}
+
+async function removeWorkspaceFolder(workspaceFolder: WorkspaceFolder): Promise<void> {
+  let folderPath = workspaceFolder.uri.fsPath;
+  let client = clients.get(folderPath);
+  if (client) {
+    clients.delete(folderPath);
+    await client.stop();
+  }
+}
+
+function findLanguageServer(basedir: string): string | null {
   try {
-    serverArgs.unshift(resolve('@glint/core/bin/glint-language-server', { basedir: folderPath }));
+    return resolve('@glint/core/bin/glint-language-server', { basedir });
   } catch {
     // Many workspaces with `tsconfig` files won't be Glint projects, so it's totally fine for us to
     // just bail out if we don't see `@glint/core`. If someone IS expecting Glint to run for this
     // project, though, we leave a message in our channel explaining why we didn't launch.
     outputChannel.appendLine(
-      `Unable to resolve @glint/core from ${folderPath} — not launching Glint for this directory.`
+      `Unable to resolve @glint/core from ${basedir} — not launching Glint for this directory.`
     );
-    return;
-  }
 
-  let serverOptions: ServerOptions = {
-    run: { command: 'node', args: serverArgs },
-    debug: { command: 'node', args: ['--nolazy', `--inspect`, ...serverArgs] },
-  };
-
-  let extensions = ['.js', '.ts', '.gjs', '.gts', '.hbs'];
-  let filePattern = `${folderPath}/**/*{${extensions.join(',')}}`;
-
-  let clientOptions: LanguageClientOptions = {
-    workspaceFolder,
-    outputChannel,
-    documentSelector: [{ scheme: 'file', pattern: filePattern }],
-    synchronize: {
-      fileEvents: workspace.createFileSystemWatcher(filePattern),
-    },
-  };
-
-  const client = new LanguageClient('glint', 'Glint', serverOptions, clientOptions);
-  const disposable = client.start();
-
-  context.subscriptions.push(disposable);
-  clients.set(folderPath, disposable);
-}
-
-function removeWorkspaceFolder(workspaceFolder: WorkspaceFolder, context: ExtensionContext): void {
-  let folderPath = workspaceFolder.uri.fsPath;
-  let client = clients.get(folderPath);
-  if (client) {
-    clients.delete(folderPath);
-    context.subscriptions.splice(context.subscriptions.indexOf(client), 1);
-    client.dispose();
+    return null;
   }
 }
