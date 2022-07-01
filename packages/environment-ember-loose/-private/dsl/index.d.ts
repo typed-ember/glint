0db57550b5cf6a643257e081823561cbfd5c9fba
Dan Freeman <dfreeman@salsify.com>
2022-07-01 17:15:36 +0200

    Support functions as helpers in environment-ember-loose

--- packages/environment-ember-loose/-private/dsl/index.d.ts
+++ packages/environment-ember-loose/-private/dsl/index.d.ts
@@ -1,4 +1,37 @@
-import './integration-declarations';
+export * from './without-function-resolution';
 
-export * from '@glint/template/-private/dsl';
-export { Globals } from './globals';
+import { ResolveOrReturn } from '@glint/template/-private/dsl';
+import {
+  DirectInvokable,
+  EmptyObject,
+  Invokable,
+  Invoke,
+  InvokeDirect,
+} from '@glint/template/-private/integration';
+
+type OptNamed<N extends Record<string, unknown> | undefined> = N extends undefined
+  ? NonNullable<N> | EmptyObject
+  : N;
+
+// Items that can be directly invoked by value
+export declare function resolve<T extends DirectInvokable>(item: T): T[typeof InvokeDirect];
+// Items whose instance type can be invoked
+export declare function resolve<Args extends unknown[], Instance extends Invokable>(
+  item: abstract new (...args: Args) => Instance
+): (...args: Parameters<Instance[typeof Invoke]>) => ReturnType<Instance[typeof Invoke]>;
+// Functions that narrow the type of their first arg
+export declare function resolve<Value, Args extends unknown[], T extends Value>(
+  item: (value: Value, ...args: Args) => value is T
+): (named: EmptyObject, value: Value, ...args: Args) => value is T;
+// Functions that have a final parameter that looks like named args
+export declare function resolve<
+  P extends unknown[],
+  N extends Record<string, unknown> | undefined,
+  T
+>(item: (...args: [...positional: P, named: N]) => T): (named: OptNamed<N>, ...positional: P) => T;
+// All other functions
+export declare function resolve<P extends unknown[], T>(
+  item: (...positional: P) => T
+): (named: EmptyObject, ...positional: P) => T;
+
+export declare const resolveOrReturn: ResolveOrReturn<typeof resolve>;
