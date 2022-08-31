33dc1ac3f87e2192073f9a84255b897ea51f7bfd
Chad Hietala <chietala@linkedin.com>
2022-08-31 09:47:41 -0400

    add tests

--- test-packages/ts-glimmerx-app/src/GreetingHeader.ts
+++ test-packages/ts-glimmerx-app/src/GreetingHeader.ts
@@ -1,17 +1,22 @@
 import Component, { hbs } from '@glimmerx/component';
 import { helper } from '@glimmerx/helper';
+import { WithBoundArgs } from '@glint/template';
 
 const or = helper(<T, U>([a, b]: [T, U]) => a || b);
 
 export interface GreetingHeaderSignature {
   Args: {
-    greeting: string;
+    greeting: WithBoundArgs<typeof TestingComponentHelper, 'name'>;
     target?: string;
   };
 }
 
 export default class GreetingHeader extends Component<GreetingHeaderSignature> {
   public static template = hbs`
-    <h1>{{@greeting}}, {{or @target 'glimmerx'}}</h1>
+    <h1>{{@greeting}}, welcome to {{or @target 'glimmerx'}}</h1>
   `;
+}
+
+export class TestingComponentHelper extends Component<{ Args: { name: string } }> {
+  static template = hbs`hello {{@name}}`;
 }
