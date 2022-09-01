e33a5f157ef179f2e8f7e3d4ef2105ffd548fc8d
Chad Hietala <chietala@linkedin.com>
2022-09-01 10:58:32 -0400

    feat: Expose Globals registry to glimmerx
    While GlimmerX is module-centric world, applications can still have
    macros that are defined by the surrounding build system. This was
    partially there with `config.additionalGlobals` but we actually
    need to expose a registry that can be merged into so that the
    globals aren't `any` types

--- test-packages/ts-glimmerx-app/types/index.d.ts
+++ test-packages/ts-glimmerx-app/types/index.d.ts
@@ -1,1 +1,13 @@
 import '@glint/environment-glimmerx';
+import type { Helper } from '@glimmerx/helper';
+declare module '@glint/environment-glimmerx/globals' {
+  export default interface Globals {
+    t: Helper<{
+      Args: {
+        Positional: string[];
+        Named: Record<string, unknown>;
+      };
+      Return: string;
+    }>;
+  }
+}
