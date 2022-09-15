14bae07a3303f54eaf4379f61dd27455e1b3a7a2
patricklx <patricklx@users.noreply.github.com>
2022-09-15 09:31:55 +0200

    Update helper.d.ts

--- packages/environment-ember-loose/-private/intrinsics/helper.d.ts
+++ packages/environment-ember-loose/-private/intrinsics/helper.d.ts
@@ -1,4 +1,4 @@
-import { AcceptsBlocks, DirectInvokable, EmptyObject } from '@glint/template/-private/integration';
+import { DirectInvokable, EmptyObject } from '@glint/template/-private/integration';
 
 export type HelperKeyword = DirectInvokable<{
   <T extends keyof Registry>(args: EmptyObject, value: T): T extends keyof Registry ? Registry[T] : never
