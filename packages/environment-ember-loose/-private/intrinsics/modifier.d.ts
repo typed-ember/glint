3ff4bccf9366693ea9da8f14c47fd83f6ec86c13
patricklx <patricklx@users.noreply.github.com>
2022-09-15 09:33:15 +0200

    Update modifier.d.ts

--- packages/environment-ember-loose/-private/intrinsics/modifier.d.ts
+++ packages/environment-ember-loose/-private/intrinsics/modifier.d.ts
@@ -1,5 +1,5 @@
 import { DirectInvokable, EmptyObject } from '@glint/template/-private/integration';
 
-export type ModifierKeyword = DirectInvokable<{
+export type ModifierHelper = DirectInvokable<{
     <T extends keyof Registry>(args: EmptyObject, value: T): T extends keyof Registry ? Registry[T] : never
   }>;
