b592c407be0f7fc92d69c4ace941a79f7efb3165
Chris Krycho <hello@chriskrycho.com>
2022-11-23 10:42:10 -0700

    Explain `GetSetMarker` shenanigans

--- packages/environment-ember-loose/-private/intrinsics/get.d.ts
+++ packages/environment-ember-loose/-private/intrinsics/get.d.ts
@@ -2,6 +2,11 @@ import { DirectInvokable } from '@glint/template/-private/integration';
 import ObjectProxy from '@ember/object/proxy';
 import '@ember/object/-private/types';
 
+// This hack lets us support both the stable/preview types from `ember-source`
+// and the classic types still live on DefinitelyTyped. If using the DT types,
+// this acts as a declaration merge for a module which is then integrated via
+// the rest of the DT types. If using the stable/preview types, this ends up
+// being a no-op.
 declare const GetSetMarker: unique symbol;
 declare module '@ember/object/-private/types' {
   interface ComputedPropertyMarker<Get, Set = Get> {
