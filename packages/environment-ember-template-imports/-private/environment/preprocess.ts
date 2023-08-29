a3ab81af8b5351ab7e0ad4302bd7c1bfbdfcfaeb
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-08-29 16:50:39 -0400

    Remove ember-template-imports in favor of content-tag

--- packages/environment-ember-template-imports/-private/environment/preprocess.ts
+++ packages/environment-ember-template-imports/-private/environment/preprocess.ts
@@ -1,14 +1,42 @@
 import { GlintExtensionPreprocess } from '@glint/core/config-types';
-import { parseTemplates } from 'ember-template-imports';
 import { GLOBAL_TAG, PreprocessData, TemplateLocation } from './common';
-
+/**
+ * We don't need to actually generate the code that would be emitted for
+ * a real app to build, we can use placeholders, so we have less offsets
+ * to worry about (precompile, setComponentTemplate, their imports, etc)
+ */
 const TEMPLATE_START = `[${GLOBAL_TAG}\``;
 const TEMPLATE_END = '`]';
 
+// NOTE: This import is a lie -- We are not real ESM, and this is compiled to CJS.
+//       ESM usage would be different here, and this is the CJS version of content-tag.
+import { Preprocessor } from 'content-tag';
+const p = new Preprocessor();
+
+interface Parsed {
+  type: 'expression' | 'class-member';
+  tagName: 'template';
+  contents: string;
+  range: {
+    start: number;
+    end: number;
+  };
+  contentRange: {
+    start: number;
+    end: number;
+  };
+  startRange: {
+    end: number;
+    start: number;
+  };
+  endRange: {
+    start: number;
+    end: number;
+  };
+}
+
 export const preprocess: GlintExtensionPreprocess<PreprocessData> = (source, path) => {
-  let templates = parseTemplates(source, path, { templateTag: 'template' }).filter(
-    (match) => match.type === 'template-tag'
-  );
+  let templates = p.parse(source, path) as Parsed[];
 
   let templateLocations: Array<TemplateLocation> = [];
   let segments: Array<string> = [];
@@ -16,11 +44,10 @@ export const preprocess: GlintExtensionPreprocess<PreprocessData> = (source, pat
   let delta = 0;
 
   for (let template of templates) {
-    let { start, end } = template;
-    let startTagLength = template.start[0].length;
-    let endTagLength = template.end[0].length;
-    let startTagOffset = start.index ?? -1;
-    let endTagOffset = end.index ?? -1;
+    let startTagLength = template.startRange.end - template.startRange.start;
+    let endTagLength = template.endRange.end - template.endRange.start;
+    let startTagOffset = template.startRange.start;
+    let endTagOffset = template.endRange.start;
 
     if (startTagOffset === -1 || endTagOffset === -1) continue;
 
