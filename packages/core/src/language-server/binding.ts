87b4e4b6de1bb3bb943c7de3f7c973e200313c76
machty <machty@gmail.com>
2024-01-11 16:09:57 +0800

    prettier

--- packages/core/src/language-server/binding.ts
+++ packages/core/src/language-server/binding.ts
@@ -177,7 +177,7 @@ export function bindLanguageServerPool({
       // (because I think that clears out all the TS preferences, including whether auto-import is enabled)
       // preferences = {
       //   includeCompletionsForModuleExports: true,
-      // };  
+      // };
 
       return server.getCompletions(textDocument.uri, position, formatting, preferences);
     });
@@ -193,11 +193,11 @@ export function bindLanguageServerPool({
         let formatting = configManager.getFormatCodeSettingsFor(language);
         let preferences = configManager.getUserSettingsFor(language);
 
-      // tmp hack -- uncomment me when testing via extension host with default TS extension disabled
-      // (because I think that clears out all the TS preferences, including whether auto-import is enabled)
-      // preferences = {
-      //   includeCompletionsForModuleExports: true,
-      // };  
+        // tmp hack -- uncomment me when testing via extension host with default TS extension disabled
+        // (because I think that clears out all the TS preferences, including whether auto-import is enabled)
+        // preferences = {
+        //   includeCompletionsForModuleExports: true,
+        // };
 
         return server.getCompletionDetails(glintItem, formatting, preferences);
       }) ?? item
