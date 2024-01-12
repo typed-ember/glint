a9cb5a273402e4acd4f75ff2a90a762436322c80
machty <machty@gmail.com>
2024-01-12 11:53:01 +0800

    fix lint

--- packages/core/src/language-server/util/previewer.ts
+++ packages/core/src/language-server/util/previewer.ts
@@ -45,54 +45,54 @@ function getTagBodyText(tag: ts.JSDocTagInfo): string | undefined {
     return '```\n' + text + '\n```';
   }
 
-  switch (tag.name) {
-    case 'example':
-      // check for caption tags, fix for #79704
-      const captionTagMatches = plain(tag.text).match(/<caption>(.*?)<\/caption>\s*(\r\n|\n)/);
-      if (captionTagMatches && captionTagMatches.index === 0) {
-        return (
-          captionTagMatches[1] +
-          '\n\n' +
-          makeCodeblock(plain(tag.text).slice(captionTagMatches[0].length))
-        );
-      } else {
-        return makeCodeblock(plain(tag.text));
-      }
-    case 'author':
-      // fix obsucated email address, #80898
-      const emailMatch = plain(tag.text).match(/(.+)\s<([-.\w]+@[-.\w]+)>/);
-
-      if (emailMatch === null) {
-        return plain(tag.text);
-      } else {
-        return `${emailMatch[1]} ${emailMatch[2]}`;
-      }
-    case 'default':
+  if (tag.name === 'example') {
+    // check for caption tags, fix for #79704
+    const captionTagMatches = plain(tag.text).match(/<caption>(.*?)<\/caption>\s*(\r\n|\n)/);
+    if (captionTagMatches && captionTagMatches.index === 0) {
+      return (
+        captionTagMatches[1] +
+        '\n\n' +
+        makeCodeblock(plain(tag.text).slice(captionTagMatches[0].length))
+      );
+    } else {
       return makeCodeblock(plain(tag.text));
+    }
+  } else if (tag.name === 'author') {
+    // fix obsucated email address, #80898
+    const emailMatch = plain(tag.text).match(/(.+)\s<([-.\w]+@[-.\w]+)>/);
+
+    if (emailMatch === null) {
+      return plain(tag.text);
+    } else {
+      return `${emailMatch[1]} ${emailMatch[2]}`;
+    }
+  } else if (tag.name === 'default') {
+    return makeCodeblock(plain(tag.text));
   }
 
   return processInlineTags(plain(tag.text));
 }
 
 export function getTagDocumentation(tag: ts.JSDocTagInfo): string | undefined {
-  switch (tag.name) {
-    case 'augments':
-    case 'extends':
-    case 'param':
-    case 'template':
-      const body = plain(tag.text || '').split(/^(\S+)\s*-?\s*/);
-      if (body?.length === 3) {
-        const param = body[1];
-        const doc = body[2];
-        const label = `*@${tag.name}* \`${param}\``;
-        if (!doc) {
-          return label;
-        }
-        return (
-          label +
-          (doc.match(/\r\n|\n/g) ? '  \n' + processInlineTags(doc) : ` — ${processInlineTags(doc)}`)
-        );
+  if (
+    tag.name === 'augments' ||
+    tag.name === 'extends' ||
+    tag.name === 'param' ||
+    tag.name === 'template'
+  ) {
+    const body = plain(tag.text || '').split(/^(\S+)\s*-?\s*/);
+    if (body?.length === 3) {
+      const param = body[1];
+      const doc = body[2];
+      const label = `*@${tag.name}* \`${param}\``;
+      if (!doc) {
+        return label;
       }
+      return (
+        label +
+        (doc.match(/\r\n|\n/g) ? '  \n' + processInlineTags(doc) : ` — ${processInlineTags(doc)}`)
+      );
+    }
   }
 
   // Generic tag
