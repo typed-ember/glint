6dcf2f8c6cdf97c5095a154bcb93d65e7d1a0047
machty <machty@gmail.com>
2024-07-03 13:54:50 -0400

    volar-2.4.0-alpha.13

--- packages/core/src/cli/run-volar-tsc.ts
+++ packages/core/src/cli/run-volar-tsc.ts
@@ -9,8 +9,16 @@ export function run(): void {
   let runExtensions = ['.js', '.ts', '.gjs', '.gts', '.hbs'];
   let cwd = process.cwd();
 
+  const options = {
+    supportedExtensions: ['.js', '.ts', '.gjs', '.gts', '.hbs'],
+    extensionsToRemove: [],
+
+    // Uncomment this to produce `basename.d.ts` files instead of `basename.gts.d.ts`
+    // extensionsToRemove: ['.gts', '.gjs'],
+  }
+
   const main = (): void =>
-    runTsc(require.resolve('typescript/lib/tsc'), runExtensions, (ts, options) => {
+    runTsc(require.resolve('typescript/lib/tsc'), options, (ts, options) => {
       const glintConfig = findConfig(cwd);
 
       // NOTE: this code used to assert in the failure of finding Glint config; I'm
