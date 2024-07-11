4f3aa176d43a481a7c1a7fa1657a5b4bd5db08c6
machty <machty@gmail.com>
2024-07-11 14:29:27 -0400

    ts plugin stub

---
+++ packages/core/src/volar/typescript-server-plugin.ts
@@ -0,0 +1,26 @@
+import { createLanguageServicePlugin } from '@volar/typescript/lib/quickstart/createLanguageServicePlugin.js';
+import { findConfig } from '../config/index.js';
+import { createEmberLanguagePlugin } from './ember-language-plugin.js';
+
+const plugin = createLanguageServicePlugin((ts, info) => {
+  const cwd = info.languageServiceHost.getCurrentDirectory();
+  const glintConfig = findConfig(cwd);
+
+  // NOTE: this code used to assert in the failure of finding Glint config; I'm
+  // not sure whether it's better to be lenient, but we were getting test failures
+  // on environment-ember-loose's `yarn run test`.
+  if (glintConfig) {
+    const gtsLanguagePlugin = createEmberLanguagePlugin(glintConfig);
+    return {
+      languagePlugins: [gtsLanguagePlugin],
+    };
+  } else {
+    return {
+      languagePlugins: [],
+    };
+  }
+});
+
+// @ts-expect-error TypeScript Plugin needs to be exported with `export =`
+// eslint-disable-next-line no-restricted-syntax
+export = plugin;
