b37e0442f23c2e8bc3f0268ee57d51218fb21922
machty <machty@gmail.com>
2025-01-13 15:00:34 -0500

    tmp change test timeouts

--- packages/vscode/__tests__/support/vscode-runner.ts
+++ packages/vscode/__tests__/support/vscode-runner.ts
@@ -11,7 +11,8 @@ export function run(
   testSubfolder: 'language-server-tests' | 'ts-plugin-tests',
 ): void {
   try {
-    let mocha = new Mocha({ color: true, slow: 3_000, timeout: 30_000 });
+    let mocha = new Mocha({ color: true, slow: 3_000, timeout: 300_000 });
+    // let mocha = new Mocha({ color: true, slow: 3_000, timeout: 30_000 });
     let tests = path.resolve(__dirname, '..').replace(/\\/g, '/');
 
     for (let testFile of glob.sync(`${tests}/${testSubfolder}/**/*.test.js`)) {
