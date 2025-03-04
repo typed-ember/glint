4911bd91ba9fabc3f41fecd91fb0c6f967079e86
machty <machty@gmail.com>
2025-03-04 11:09:49 -0500

    prettier

--- packages/typescript-plugin/src/typescript-server-plugin.ts
+++ packages/typescript-plugin/src/typescript-server-plugin.ts
@@ -1,4 +1,4 @@
-const { createJiti } = require("jiti");
+const { createJiti } = require('jiti');
 const jiti = createJiti(__filename);
 
 const {
@@ -12,7 +12,7 @@ const plugin = createLanguageServicePlugin((_ts: typeof import('typescript'), in
    * that TypeScript plugins are written in CommonJS, which poses issues with
    * having Glint be authored in ESM due to the requirement that typically `await import`
    * is required to load ESM modules from CJS. But with jiti we can synchronously load the ESM
-   * modules from CJS which lets us avoid a ton of hacks and complexity we (or Volar) 
+   * modules from CJS which lets us avoid a ton of hacks and complexity we (or Volar)
    * would otherwise have to write to bridge the sync/async APIs.
    */
   const glintCore = jiti('@glint/core');
