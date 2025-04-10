6286d96f7a94cbb97f589bd8103eff8ff57eda7f
Patrick Pircher <patricklx@users.noreply.github.com>
2025-04-11 00:22:34 +0900

    xmlns is not required

--- test-packages/package-test-core/__tests__/language-server/diagnostics.test.ts
+++ test-packages/package-test-core/__tests__/language-server/diagnostics.test.ts
@@ -644,8 +644,7 @@ describe('Language Server: Diagnostics (ts plugin)', () => {
     export default class MyComponent {
       <template>
         <svg version="1.1"
-            width="300" height="200"
-            xmlns="http://www.w3.org/2000/svg">
+            width="300" height="200">
 
           <rect width="100%" height="100%" fill="red" class="foo" />
         </svg>
