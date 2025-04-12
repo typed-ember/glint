680e634fe111925e8033a7b9d70dad9e19170989
Alex Matchneer <machty@gmail.com>
2025-04-12 06:30:47 -0400

    Move .gts/.gjs import hacks to TS Plugin (#890)

--- test-packages/package-test-core/__tests__/language-server/imports.test.ts
+++ test-packages/package-test-core/__tests__/language-server/imports.test.ts
@@ -8,7 +8,7 @@ import { stripIndent } from 'common-tags';
 describe('Language Server: Imports', () => {
   afterEach(teardownSharedTestWorkspaceAfterEach);
 
-  test('support moduleResolution=bundler index.gts resolution from gts file with <template>', async () => {
+  test('support moduleResolution=bundler importing from gts file with <template>', async () => {
     const code = stripIndent`
       import Colocated from './colocated';
 
@@ -26,7 +26,7 @@ describe('Language Server: Imports', () => {
     expect(diagnostics).toMatchInlineSnapshot(`[]`);
   });
 
-  test('support moduleResolution=bundler index.gts resolution from gts file with no <template>', async () => {
+  test('support moduleResolution=bundler importing from gts file with no <template>', async () => {
     const code = stripIndent`
       import Colocated from './colocated';
 
@@ -35,6 +35,22 @@ describe('Language Server: Imports', () => {
 
     const diagnostics = await requestDiagnostics(
       'ts-template-imports-app/src/ephemeral-index.gts',
+      'glimmer-ts',
+      code,
+    );
+
+    expect(diagnostics).toMatchInlineSnapshot(`[]`);
+  });
+
+  test('support moduleResolution=bundler importing from vanilla TS file', async () => {
+    const code = stripIndent`
+      import Colocated from './colocated';
+
+      export default Colocated;
+    `;
+
+    const diagnostics = await requestDiagnostics(
+      'ts-template-imports-app/src/ephemeral-vanilla-typescript.ts',
       'glimmer-ts',
       code,
     );
--- test-packages/ts-plugin-test-app/src/auto-import-test-exporter.gts
+++
@@ -1,1 +0,0 @@
-export const autoImportTestExporterValue = 123;
