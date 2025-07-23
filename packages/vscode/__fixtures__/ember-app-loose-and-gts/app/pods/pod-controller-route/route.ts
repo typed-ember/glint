7469c295f717f9c5f0ae9ffc8b4e03a3665e34c7
Alex Matchneer <machty@gmail.com>
2025-07-23 13:46:39 -0400

    rm old Pods components (#923)

--- packages/vscode/__fixtures__/ember-app-loose-and-gts/app/pods/pod-controller-route/route.ts
+++
@@ -1,7 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default class PodControllerRoute extends Route {
-  async model(): Promise<{ message: string }> {
-    return { message: 'hello' };
-  }
-}
--- packages/vscode/__fixtures__/ember-app/app/pods/pod-controller-route/controller.ts
+++
@@ -1,5 +0,0 @@
-import Controller from '@ember/controller';
-
-export default class ClassicController extends Controller {
-  declare model: { message: string };
-}
