ab8b00ab3fd3b37267b98595f5198c4b1400bd2f
machty <machty@gmail.com>
2025-07-23 13:40:32 -0400

    rm old Pods components

--- packages/vscode/__fixtures__/ember-app/app/pods/pod-lone-route/route.ts
+++
@@ -1,7 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default class PodLoneRoute extends Route {
-  async model(): Promise<{ message: string }> {
-    return { message: 'hello' };
-  }
-}
