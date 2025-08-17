c40c8b4107a420f35585b7190122bb9b655f2e6d
machty <machty@gmail.com>
2025-08-16 20:27:20 -0400

    move typecheck scripts around

--- packages/environment-ember-template-imports/vitest.config.ts
+++ packages/environment-ember-template-imports/vitest.config.ts
@@ -2,6 +2,19 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   esbuild: {
-    target: 'node14',
+    target: 'node18',
+  },
+  test: {
+    exclude: [
+      // type tests are run as part of the test:typecheck script, but it causes issues if vitest tries to evaluate these files
+      '__tests__/type-tests',
+
+      // Default `exclude` options (https://vitest.dev/config/#exclude)
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
+    ],
   },
 });
