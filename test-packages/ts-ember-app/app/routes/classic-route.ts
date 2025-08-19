866ace3f8ffd296e8705f48144c44b1ace0f2e46
machty <machty@gmail.com>
2025-08-19 04:09:54 -0400

    rm ts-ember-app

--- test-packages/ts-ember-app/app/routes/classic-route.ts
+++
@@ -1,7 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default class ClassicRoute extends Route {
-  override async model(): Promise<{ foo: string }> {
-    return { foo: 'hi' };
-  }
-}
