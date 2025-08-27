502b6fb69b9043e0d508771a5afab8f279b9bb94
machty <machty@gmail.com>
2025-08-27 12:05:59 -0400

    reinstate `on` test

--- test-packages/package-test-core/__tests__/type-tests/intrinsics/on.test.ts
+++ test-packages/package-test-core/__tests__/type-tests/intrinsics/on.test.ts
@@ -1,44 +1,40 @@
 // Commented because failing and I'm not sure ETI needs these kinds of "intrinsics" to be tested
 
-// import {
-//   Globals,
-//   NamedArgsMarker,
-//   applyModifier,
-//   resolve,
-// } from '@glint/core/-private/dsl';
-// import { expectTypeOf } from 'expect-type';
+import { on as onImport } from '@ember/modifier';
+import { NamedArgsMarker, applyModifier, resolve } from '@glint/core/-private/dsl';
+import { expectTypeOf } from 'expect-type';
 
-// const on = resolve(Globals['on']);
-// const el = document.createElement('div');
+const on = resolve(onImport);
+const el = document.createElement('div');
 
-// on(el, 'click', () => {}, {
-//   // @ts-expect-error: extra named arg
-//   foo: 'bar',
-//   ...NamedArgsMarker,
-// });
+on(el, 'click', () => {}, {
+  // @ts-expect-error: extra named arg
+  foo: 'bar',
+  ...NamedArgsMarker,
+});
 
-// // @ts-expect-error: missing positional arg
-// on(el, 'click');
+// @ts-expect-error: missing positional arg
+on(el, 'click');
 
-// // @ts-expect-error: extra positional arg
-// on(el, 'click', () => {}, 'hello');
+// @ts-expect-error: extra positional arg
+on(el, 'click', () => {}, 'hello');
 
-// on(el, 'scroll', () => {}, { capture: true, once: true, passive: true, ...NamedArgsMarker });
+on(el, 'scroll', () => {}, { capture: true, once: true, passive: true, ...NamedArgsMarker });
 
-// on(el, 'unknown', (event) => {
-//   expectTypeOf(event).toEqualTypeOf<Event>();
-// });
+on(el, 'unknown', (event) => {
+  expectTypeOf(event).toEqualTypeOf<Event>();
+});
 
-// on(el, 'click', (event) => {
-//   expectTypeOf(event).toEqualTypeOf<MouseEvent>();
-// });
+on(el, 'click', (event) => {
+  expectTypeOf(event).toEqualTypeOf<MouseEvent>();
+});
 
-// on(el, 'keyup', (event) => {
-//   expectTypeOf(event).toEqualTypeOf<KeyboardEvent>();
-// });
+on(el, 'keyup', (event) => {
+  expectTypeOf(event).toEqualTypeOf<KeyboardEvent>();
+});
 
-// applyModifier(on(el, 'click', () => {}));
+applyModifier(on(el, 'click', () => {}));
 
-// applyModifier(on(new SVGRectElement(), 'click', () => {}));
+applyModifier(on(new SVGRectElement(), 'click', () => {}));
 
-// applyModifier(on(new Element(), 'click', () => {}));
+applyModifier(on(new Element(), 'click', () => {}));
