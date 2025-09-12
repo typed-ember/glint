a507c376088e9e3943ccab9a21b11f9a36f67e24
machty <machty@gmail.com>
2025-09-12 16:43:10 -0400

    expose emitAttribute errors when reverse-source-mapping

--- packages/core/src/transform/template/template-to-typescript.ts
+++ packages/core/src/transform/template/template-to-typescript.ts
@@ -846,7 +846,15 @@ export function templateToTypescript(
         (attr) => !attr.name.startsWith('@') && attr.name !== SPLATTRIBUTES,
       );
 
-      mapper.text('__glintDSL__.applyAttributes(__glintY__.element, {');
+      if (attributes.length === 0) return;
+
+      mapper.text('__glintDSL__.applyAttributes(');
+
+      mapper.forNode(node, () => {
+        mapper.text('__glintY__.element');
+      });
+
+      mapper.text(', {');
       const attrsSpan = node.openTag
         .withStart(node.path.loc.getEnd())
         .withEnd(node.openTag.getEnd().move(-1));
