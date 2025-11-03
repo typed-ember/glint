f9a328edfd27e80b87f6a9751f93df31a3242a46
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-11-03 14:32:47 -0500

    Add another type test

---
+++ packages/template/__tests__/private/ElementForTagName.test.ts
@@ -0,0 +1,25 @@
+import '@glint/template';
+
+import { expectTypeOf } from 'expect-type';
+import type { ElementForTagName } from '../../-private/dsl/types';
+
+class MyCustomElement extends HTMLElement {
+    propNum!: number;
+    propStr!: string;
+}
+
+declare global {
+    interface GlintCustomElements {
+        'my-custom-element-element-for-tag-name': MyCustomElement;
+    }
+}
+
+{
+    type X = ElementForTagName<'my-custom-element-element-for-tag-name'>;
+
+    expectTypeOf<X>().toEqualTypeOf<MyCustomElement>();
+    expectTypeOf<X['propNum']>().toEqualTypeOf<number>();
+    expectTypeOf<X['propStr']>().toEqualTypeOf<string>();
+
+    expectTypeOf<X['propNum']>().not.toEqualTypeOf<string>();
+}
