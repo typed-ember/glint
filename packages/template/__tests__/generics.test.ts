8d1871145002ca300f36f5a4a143db87951c6744
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-01-08 18:36:07 -0500

    More tests, more exploration

--- packages/template/__tests__/generics.test.ts
+++ packages/template/__tests__/generics.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-inner-declarations */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import '@glint/ember-tsc/types';
 import Component from '@glimmer/component';
@@ -6,12 +7,23 @@ import { expectTypeOf } from 'expect-type';
 import {
   emitComponent,
   resolve,
+  resolveComponent,
+  resolveModifier,
   NamedArgsMarker,
   applyAttributes,
   emitElement,
   applyModifier,
 } from '../-private/dsl';
 import { ModifierLike } from '../-private';
+import {
+  AnyFunction,
+  ComponentReturn,
+  Invoke,
+  Element,
+  Blocks,
+  Invokable,
+  InvokableInstance,
+} from '../-private/integration';
 
 {
   /**
@@ -21,27 +33,94 @@ import { ModifierLike } from '../-private';
   type Signature<Args> = Args extends { href: string }
     ? {
         Element: HTMLAnchorElement;
-        Args: { href: string };
+        Args: Args;
         Blocks: { default: [] };
       }
     : {
         Element: HTMLDivElement;
-        Args: { foo?: number };
+        Args: { div: true } & Args;
         Blocks: { default: [] };
       };
 
   class Link<Args> extends Component<Signature<Args>> {}
 
+  // Manual sanity checking for the above signature
+  {
+    type X = ReturnType<Link<{ href: string }>[typeof Invoke]>;
+    type Y = X[typeof Element];
+
+    expectTypeOf<Y>().toEqualTypeOf<HTMLAnchorElement>();
+
+    type A = ReturnType<Link<{ foo: string }>[typeof Invoke]>;
+    type B = A[typeof Element];
+
+    expectTypeOf<B>().toEqualTypeOf<HTMLDivElement>();
+  }
+  // Manual sanity checking for the above signature
+  // This proves that args can be associated to the component,
+  // and that the return value can derive from the args.
+  // This is the proof of concept for the part of glint/template that supports generics / narrowing.
+  // {
+  //   type GenericSignature = { Args: unknown, Element: Element };
+  //   type Sig2<Args> = Args extends { href: string } ? { Args: Args, Element: HTMLAnchorElement } : { Args: Args, Element: HTMLDivElement };
+
+  //   function test<Args, Sig extends GenericSignature>(y: Args): Sig['Element'] {
+  //     return 0 as unknown as Sig['Element'];
+  //   }
+  //   interface Link2<Args> extends Sig2<Args> {}
+  //   class Link2<Args> {}
+  //   let n = test({ href: 'test' });
+  //   expectTypeOf(n).toEqualTypeOf<HTMLAnchorElement>();
+  //   let m = test({ foo: 123 });
+  //   expectTypeOf(m).toEqualTypeOf<HTMLDivElement>();
+
+  //   function elementOf<Args, Comp extends abstract new (owner: any, args: Args) => any>(args: Args, comp: Comp): ComponentReturn<any, Element> {
+  //       return 0 as any;
+  //     }
+
+  //   let el = elementOf<Link>({ href: 'test'}, Link);
+
+  //   expectTypeOf(el[Element]).toEqualTypeOf<HTMLAnchorElement>();
+  // }
+
   // Renders as anchor
   {
-    const __glintY__ = emitComponent(
-      resolve(Link)({
-        // Error unexpected
-        href: 'https://www.auditboard.com/platform/ai',
-        ...NamedArgsMarker,
-      }),
-    );
+    const fn = resolve(Link);
+    const resolved = fn<{ href: string }>({
+      // Error unexpected
+      href: 'https://www.auditboard.com/platform/ai',
+      ...NamedArgsMarker,
+    });
 
+    expectTypeOf(resolved).not.toBeAny();
+    expectTypeOf(resolved).not.toEqualTypeOf<AnyFunction>();
+    expectTypeOf(resolved).not.toBeUnknown();
+
+    const __glintY__ = emitComponent(resolved);
+
+    expectTypeOf(__glintY__.element).not.toBeAny();
+    expectTypeOf(__glintY__.element).toEqualTypeOf<HTMLAnchorElement>();
+
+    // element here "should" be derived from args above, but it is not
+    applyAttributes(__glintY__.element, {
+      // error unexpected
+      target: '_blank',
+    });
+  }
+  {
+    const resolved = resolveComponent(Link, {
+      // Error unexpected
+      href: 'https://www.auditboard.com/platform/ai',
+      ...NamedArgsMarker,
+    });
+
+    expectTypeOf(resolved).not.toBeAny();
+    expectTypeOf(resolved).not.toEqualTypeOf<AnyFunction>();
+    expectTypeOf(resolved).not.toBeUnknown();
+
+    const __glintY__ = emitComponent(resolved);
+
+    expectTypeOf(__glintY__.element).not.toBeAny();
     expectTypeOf(__glintY__.element).toEqualTypeOf<HTMLAnchorElement>();
 
     // element here "should" be derived from args above, but it is not
@@ -54,13 +133,87 @@ import { ModifierLike } from '../-private';
   // Renders as div
   {
     const __glintY__ = emitComponent(
-      resolve(Link)({
+      resolveComponent(Link, {
         // @ts-expect-error: wrong arg type (deliberate)
         href: 2,
         ...NamedArgsMarker,
       }),
     );
 
+    expectTypeOf(__glintY__.element).not.toBeAny();
+    expectTypeOf(__glintY__.element).toEqualTypeOf<HTMLDivElement>();
+
+    // element here "should" be derived from args above, but it is not
+    applyAttributes(__glintY__.element, {
+      // @ts-expect-error: target not valid on div
+      target: '_blank',
+    });
+  }
+}
+
+{
+  /**
+   * A Link component that can sometimes be a div, if the href is missing
+   * but where the condition is on Args, not the whole signature
+   */
+
+  interface Signature<Args> {
+    Element: Args extends { href: string } ? HTMLAnchorElement : HTMLDivElement;
+    Args: Args;
+    Blocks: { default: [] };
+  }
+
+  class Link<Args> extends Component<Signature<Args>> {}
+
+  // Manual sanity checking for the above signature
+  {
+    type X = ReturnType<Link<{ href: string }>[typeof Invoke]>;
+    type Y = X[typeof Element];
+
+    expectTypeOf<Y>().toEqualTypeOf<HTMLAnchorElement>();
+
+    type A = ReturnType<Link<{ foo: string }>[typeof Invoke]>;
+    type B = A[typeof Element];
+
+    expectTypeOf<B>().toEqualTypeOf<HTMLDivElement>();
+  }
+
+  // Renders as anchor
+  {
+    const fn = resolve(Link);
+    const resolved = fn({
+      // Error unexpected
+      href: 'https://www.auditboard.com/platform/ai',
+      ...NamedArgsMarker,
+    });
+
+    expectTypeOf(resolved).not.toBeAny();
+    expectTypeOf(resolved).not.toEqualTypeOf<AnyFunction>();
+    expectTypeOf(resolved).not.toBeUnknown();
+
+    const __glintY__ = emitComponent(resolved);
+
+    expectTypeOf(__glintY__.element).not.toBeAny();
+    expectTypeOf(__glintY__.element).toEqualTypeOf<HTMLAnchorElement>();
+
+    // element here "should" be derived from args above, but it is not
+    applyAttributes(__glintY__.element, {
+      // error unexpected
+      target: '_blank',
+    });
+  }
+
+  // Renders as div
+  {
+    const __glintY__ = emitComponent(
+      resolveComponent(Link, {
+        // @ts-expect-error: wrong arg type (deliberate)
+        href: 2,
+        ...NamedArgsMarker,
+      }),
+    );
+
+    expectTypeOf(__glintY__.element).not.toBeAny();
     expectTypeOf(__glintY__.element).toEqualTypeOf<HTMLDivElement>();
 
     // element here "should" be derived from args above, but it is not
@@ -93,13 +246,14 @@ import { ModifierLike } from '../-private';
   // Renders as input
   {
     const __glintY__ = emitComponent(
-      resolve(SomeInput)({
+      resolveComponent(SomeInput, {
         // Error unexpected
         value: 'test',
         ...NamedArgsMarker,
       }),
     );
 
+    expectTypeOf(__glintY__.element).not.toBeAny();
     expectTypeOf(__glintY__.element).toEqualTypeOf<HTMLInputElement>();
 
     // element here "should" be derived from args above, but it is not
@@ -114,13 +268,14 @@ import { ModifierLike } from '../-private';
   // Renders as textarea
   {
     const __glintY__ = emitComponent(
-      resolve(SomeInput)({
+      resolveComponent(SomeInput, {
         // Error unexpected
         html: 'https://www.auditboard.com/platform/ai',
         ...NamedArgsMarker,
       }),
     );
 
+    expectTypeOf(__glintY__.element).not.toBeAny();
     expectTypeOf(__glintY__.element).toEqualTypeOf<HTMLTextAreaElement>();
 
     // element here "should" be derived from args above, but it is not
@@ -177,7 +332,7 @@ import { ModifierLike } from '../-private';
     expectTypeOf(img.element).toEqualTypeOf<HTMLImageElement>();
 
     applyModifier(
-      resolve(ImageModifierClass)(img.element, {
+      resolveModifier(ImageModifierClass)(img.element, {
         // Correct: no error expected because the img element has a src attribute
         src: 'bar',
         ...NamedArgsMarker,
@@ -185,7 +340,7 @@ import { ModifierLike } from '../-private';
     );
 
     applyModifier(
-      resolve(ImageModifierClass)(canvas.element, {
+      resolveModifier(ImageModifierClass)(canvas.element, {
         // Correct: no error expected because the canvas element has width and height attributes
         width: 200,
         height: 100,
@@ -196,10 +351,44 @@ import { ModifierLike } from '../-private';
     );
 
     applyModifier(
-      resolve(ImageModifierClass)(
+      resolveModifier(ImageModifierClass)(
         // @ts-expect-error: wrong element type, expects image or canvas
         div.element,
       ),
     );
   }
+}
+
+/**
+ * Modifiers with generic args, but static element
+ */
+{
+  interface Area<V> {
+    area: V;
+  }
+
+  type PositionalArgs<V> = [area: Area<V>, data: V[]];
+
+  interface D3AreaSignature<V> {
+    Element: SVGPathElement;
+    Args: {
+      Positional: PositionalArgs<V>;
+    };
+  }
+  interface DefaultSignature {
+    Element: Element;
+  }
+  interface Modifier<T> extends InstanceType<ModifierLike<T>> {}
+  class Modifier<T = DefaultSignature> {
+    constructor(args: T) {}
+  }
+
+  class D3Area<V> extends Modifier<D3AreaSignature<V>> {}
+
+  let svgPath = emitElement('path');
+
+  let resolved = resolve(D3Area);
+  let withElement = resolved(svgPath.element, { area: 42 }, [1, 2, 3]);
+
+  applyModifier(withElement);
 }
