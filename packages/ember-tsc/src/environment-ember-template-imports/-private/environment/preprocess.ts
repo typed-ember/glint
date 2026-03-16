8a3ca509598bdccaa0aed8e0c5eab36f042fedb9
Johan Røed <johanroed@gmail.com>
2026-03-16 14:55:43 +0100

    Upgrade content-tag to v4 and simplify template preprocessing

--- packages/ember-tsc/src/environment-ember-template-imports/-private/environment/preprocess.ts
+++ packages/ember-tsc/src/environment-ember-template-imports/-private/environment/preprocess.ts
@@ -8,63 +8,41 @@ import { Preprocessor } from 'content-tag';
 const p = new Preprocessor();
 
 export const preprocess: GlintExtensionPreprocess<PreprocessData> = (source, path) => {
-  // NOTE: https://github.com/embroider-build/content-tag/issues/45
-  //       All indicies are byte-index, not char-index.
   let templates = p.parse(source, { filename: path });
 
   let templateLocations: Array<TemplateLocation> = [];
   let contents = '';
-  let sourceOffsetBytes = 0;
-  let deltaBytes = 0;
-
-  let sourceBuffer = getBuffer(source);
+  let sourceOffset = 0;
 
   for (let template of templates) {
-    let startTagLengthBytes = template.startRange.end - template.startRange.start;
-    let endTagLengthBytes = template.endRange.end - template.endRange.start;
-    let startTagOffsetBytes = template.startRange.start;
-    let endTagOffsetBytes = template.endRange.start;
-    let transformedStartBytes = startTagOffsetBytes - deltaBytes;
-    /**
-     * TODO: we want content-tag to manage all this for us, as managing indicies
-     *       can be error-prone.
-     *
-     * SEE: https://github.com/embroider-build/content-tag/issues/39#issuecomment-1832443310
-     */
-    let prefixingSegment = sourceBuffer.subarray(sourceOffsetBytes, startTagOffsetBytes);
-    contents = contents.concat(prefixingSegment.toString());
-    contents = contents.concat(TEMPLATE_START);
+    let startTagOffset = template.startRange.startUtf16Codepoint;
+    let startTagEnd = template.startRange.endUtf16Codepoint;
+    let endTagOffset = template.endRange.startUtf16Codepoint;
+    let endTagEnd = template.endRange.endUtf16Codepoint;
 
-    // For TEMPLATE_START & TEMPLATE_END, characters === bytes
-    deltaBytes += startTagLengthBytes - TEMPLATE_START.length;
+    contents += source.slice(sourceOffset, startTagOffset);
 
-    let transformedEnd = endTagOffsetBytes - deltaBytes + TEMPLATE_END.length;
-    let templateContentSegment = sourceBuffer.subarray(
-      startTagOffsetBytes + startTagLengthBytes,
-      endTagOffsetBytes,
-    );
-    let templateContentSegmentString = templateContentSegment.toString();
-    let escapedTemplateContentSegment = templateContentSegmentString
-      .replaceAll('${{', '\\${{')
-      .replaceAll('`', '\\`');
-    deltaBytes += templateContentSegmentString.length - escapedTemplateContentSegment.length;
+    let transformedStart = contents.length;
+    contents += TEMPLATE_START;
 
-    contents = contents.concat(escapedTemplateContentSegment);
-    contents = contents.concat(TEMPLATE_END);
-    deltaBytes += endTagLengthBytes - TEMPLATE_END.length;
+    let templateContent = source.slice(startTagEnd, endTagOffset);
+    let escapedContent = templateContent.replaceAll('${{', '\\${{').replaceAll('`', '\\`');
+    contents += escapedContent;
+    contents += TEMPLATE_END;
+    let transformedEnd = contents.length;
 
-    sourceOffsetBytes = endTagOffsetBytes + endTagLengthBytes;
+    sourceOffset = endTagEnd;
     templateLocations.push({
-      startTagOffset: byteToCharIndex(source, startTagOffsetBytes),
-      endTagOffset: byteToCharIndex(source, endTagOffsetBytes),
-      startTagLength: byteToCharIndex(source, startTagLengthBytes),
-      endTagLength: byteToCharIndex(source, endTagLengthBytes),
-      transformedStart: byteToCharIndex(contents, transformedStartBytes),
-      transformedEnd: byteToCharIndex(contents, transformedEnd),
+      startTagOffset,
+      endTagOffset,
+      startTagLength: startTagEnd - startTagOffset,
+      endTagLength: endTagEnd - endTagOffset,
+      transformedStart,
+      transformedEnd,
     });
   }
 
-  contents = contents.concat(sourceBuffer.subarray(sourceOffsetBytes).toString());
+  contents += source.slice(sourceOffset);
   return {
     contents,
     data: {
@@ -72,19 +50,3 @@ export const preprocess: GlintExtensionPreprocess<PreprocessData> = (source, pat
     },
   };
 };
-
-function byteToCharIndex(str: string, byteOffset: number): number {
-  const buf = getBuffer(str);
-  return buf.subarray(0, byteOffset).toString().length;
-}
-
-const BufferMap = new Map();
-
-function getBuffer(str: string): Buffer {
-  let buf = BufferMap.get(str);
-  if (!buf) {
-    buf = Buffer.from(str);
-    BufferMap.set(str, buf);
-  }
-  return buf;
-}
